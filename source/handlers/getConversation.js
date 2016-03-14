15d6da5baf51a5a8ae4dc006456cea7a3db1f822
Guido Schmitz <hello@guidoschmitz.co>
2016-03-14 11:51:16 +0100

    Removed includes

--- source/handlers/getConversation.js
+++ source/handlers/getConversation.js
@@ -1,11 +1,9 @@
 import Conversation from 'models/Conversation';
-import User from 'models/User';
 
 module.exports = (req, reply) => {
   Conversation.find(
     {
       where: { id: req.params.id },
-      include: [User],
     }
   ).then(conversation => {
     reply({ data: conversation });
--- source/handlers/postConversation.js
+++ source/handlers/postConversation.js
@@ -1,13 +1,9 @@
-import { Users } from 'models/ConversationUser';
 import Conversation from 'models/Conversation';
 
 module.exports = (req, reply) => {
   Conversation.create({
     type: req.payload.type,
-    Users: [
-      { id: 1 },
-    ],
-  }, { include: [Users] }).then(conversation => {
+  }).then(conversation => {
     reply({ data: conversation });
   }).catch(error => {
     reply({ message: error.message, errors: error.errors });
