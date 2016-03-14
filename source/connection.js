adad9047cdd78fccc0845f7fb573757a8359cd69
Guido Schmitz <hello@guidoschmitz.co>
2016-03-14 14:31:26 +0100

    Get dialect from config

--- source/connection.js
+++ source/connection.js
@@ -1,9 +1,6 @@
 import Sequelize from 'sequelize';
 import config from 'database.json';
 
-const { host, database, username, password } = config[process.env.NODE_ENV];
+const { host, database, username, password, dialect } = config[process.env.NODE_ENV];
 
-export default new Sequelize(database, username, password, {
-  host,
-  dialect: 'mysql',
-});
+export default new Sequelize(database, username, password, { host, dialect });
