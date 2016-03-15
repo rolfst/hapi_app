4acfc155e2d9fde73508c0cf259ac88885c1b380
Guido Schmitz <hello@guidoschmitz.co>
2016-03-15 11:26:31 +0100

    installed tape + faucet and setup boilerplate

---
+++ tests/unit/index.js
@@ -0,0 +1,11 @@
+import test from 'tape';
+
+test('test timing', assert => {
+  assert.equals(1, 1);
+  assert.end();
+});
+
+test('test timing 2', assert => {
+  assert.equals(1, 2);
+  assert.end();
+});
