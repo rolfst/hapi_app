da71bec15b650751f11f439c87b4b05ae05a9faa
Guido Schmitz <hello@guidoschmitz.co>
2016-03-15 11:09:24 +0100

    Code for creating a message

--- source/handlers/createMessage.js
+++ source/handlers/createMessage.js
@@ -2,6 +2,17 @@ import Conversation from 'models/Conversation';
 import Message from 'models/Message';
 
 module.exports = (req, reply) => {
-  console.log(req.payload);
-  reply('ok');
+  Conversation.findById(req.params.id).then(conversation => {
+    if (!conversation) reply('Not found.');
+
+    // const message = messageFactory.buildForConversation(conversation.id, req.payload.body);
+
+    return Message.create({
+      parentId: conversation.id,
+      parentType: 'FlexAppeal\\Entities\\Conversation',
+      text: req.payload.body,
+      createdBy: 2,
+      messageType: 'default',
+    });
+  }).then(createdMessage => reply({ data: createdMessage }));
 };
