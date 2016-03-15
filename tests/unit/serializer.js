0094ffc6e6a17c94b2480c53633b9986ba0e4de8
Guido Schmitz <hello@guidoschmitz.co>
2016-03-15 21:28:40 +0100

    Updated tests

--- tests/unit/serializer.js
+++ tests/unit/serializer.js
@@ -3,11 +3,13 @@ import messageSerializer from 'serializers/message';
 import buildRelations from 'utils/buildRelations';
 
 const resource = {
+  type: 'messages',
   id: 1,
   text: 'Text of a message.',
   createdBy: 2,
   created_at: '2016-02-29T20:14:48+0100',
   updated_at: '2016-02-29T20:14:48+0100',
+  Comments: [{ id: 5 }, { id: 12 }],
 };
 
 describe('Serializers', () => {
@@ -29,9 +31,7 @@ describe('Serializers', () => {
 
   it('get output for single resource with relationships', () => {
     assert.deepEqual(messageSerializer(resource, {
-      relations: [{
-        type: 'comments',
-        ids: [5, 12] }],
+      relations: ['comments'],
     }), {
       type: 'messages',
       id: 1,
@@ -46,6 +46,10 @@ describe('Serializers', () => {
       },
       relationships: {
         comments: {
+          links: {
+            self: '/messages/1/relationships/comments',
+            related: '/messages/1/comments',
+          },
           data: [{ type: 'comments', id: 5 }, { type: 'comments', id: 12 }],
         },
       },
@@ -53,15 +57,30 @@ describe('Serializers', () => {
   });
 
   it('builds multiple relations', () => {
-    const actual = buildRelations([{ type: 'user', ids: [5, 12] }, { type: 'comments', ids: [6, 9] }]);
+    const actual = buildRelations(['user', 'comments'], {
+      type: 'messages',
+      id: 1,
+      value: 'Foo',
+      baz: 'Bar',
+      User: {
+        id: 5,
+      },
+      Comments: [{ id: 6 }, { id: 9 }],
+    });
+
     const expected = {
       user: {
-        data: [
-          { type: 'user', id: 5 },
-          { type: 'user', id: 12 },
-        ],
+        links: {
+          self: '/messages/1/relationships/user',
+          related: '/messages/1/user',
+        },
+        data: { type: 'user', id: 5 },
       },
       comments: {
+        links: {
+          self: '/messages/1/relationships/comments',
+          related: '/messages/1/comments',
+        },
         data: [
           { type: 'comments', id: 6 },
           { type: 'comments', id: 9 },
@@ -73,9 +92,20 @@ describe('Serializers', () => {
   });
 
   it('builds relation with single id', () => {
-    const actual = buildRelations([{ type: 'user', ids: [5] }]);
+    const actual = buildRelations(['user'], {
+      type: 'messages',
+      id: 1,
+      User: {
+        id: 5,
+      },
+    });
+
     const expected = {
       user: {
+        links: {
+          self: '/messages/1/relationships/user',
+          related: '/messages/1/user',
+        },
         data: { type: 'user', id: 5 },
       },
     };
