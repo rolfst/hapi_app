871ca6c07a4527093ee81705a21168ab33f58f19
Guido Schmitz <hello@guidoschmitz.co>
2016-03-16 11:31:31 +0100

    Added the include to test the output

--- source/handlers/getConversation.js
+++ source/handlers/getConversation.js
@@ -1,11 +1,14 @@
 import Conversation from 'models/Conversation';
+import Message from 'models/Message';
+import respondWithItem from 'utils/respondWithItem';
+import conversationSerializer from 'serializers/conversation';
 
 module.exports = (req, reply) => {
-  Conversation.find(
-    {
-      where: { id: req.params.id },
-    }
-  ).then(conversation => {
-    reply({ data: conversation });
+  Conversation.findById(req.params.id, {
+    include: [{ model: Message, attributes: ['id'] }],
+  }).then(conversation => {
+    reply(respondWithItem(conversation, conversationSerializer, {
+      relations: ['messages'],
+    }));
   });
 };
