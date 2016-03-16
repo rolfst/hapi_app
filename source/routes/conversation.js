a7120dccdfedab55b77801e6e373f9dd20ef9bc9
Guido Schmitz <hello@guidoschmitz.co>
2016-03-16 13:30:04 +0100

    Add auth strategy to routes

--- source/routes/conversation.js
+++ source/routes/conversation.js
@@ -6,23 +6,31 @@ export default [
     method: 'GET',
     path: '/conversations',
     handler: require('handlers/getConversations'),
+    config: {
+      auth: 'default',
+    },
   }, {
     method: 'GET',
     path: '/conversations/{id}',
     handler: require('handlers/getConversation'),
     config: {
       validate: conversation.detail,
+      auth: 'default',
     },
   }, {
     method: 'GET',
     path: '/conversations/{id}/messages',
     handler: require('handlers/getMessages'),
+    config: {
+      auth: 'default',
+    },
   }, {
     method: 'POST',
     path: '/conversations/{id}/messages',
     handler: require('handlers/createMessage'),
     config: {
       validate: message.create,
+      auth: 'default',
     },
   }, {
     method: 'POST',
@@ -30,6 +38,7 @@ export default [
     handler: require('handlers/postConversation'),
     config: {
       validate: conversation.create,
+      auth: 'default',
     },
   }, {
     method: 'DELETE',
@@ -37,6 +46,7 @@ export default [
     handler: require('handlers/deleteConversation'),
     config: {
       validate: conversation.delete,
+      auth: 'default',
     },
   },
 ];
--- source/routes/user.js
+++ source/routes/user.js
@@ -5,12 +5,16 @@ export default [
     method: 'GET',
     path: '/users',
     handler: require('handlers/getUsers'),
+    config: {
+      auth: 'default',
+    },
   }, {
     method: 'POST',
     path: '/users',
     handler: require('handlers/postUser'),
     config: {
       validate: user.create,
+      auth: 'default',
     },
   },
 ];
