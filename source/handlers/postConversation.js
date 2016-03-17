9177637a7f9b0da51867d07ac8bce78e17c2667a
Liam <liam.hubers@gmail.com>
2016-03-17 21:30:46 +0100

    Started with create conversation call

--- source/handlers/postConversation.js
+++ source/handlers/postConversation.js
@@ -1,11 +1,18 @@
 import Conversation from 'models/Conversation';
+import { destructPayload } from 'services/payload';
+
+const values = ['type', 'users'];
 
 module.exports = (req, reply) => {
-  Conversation.create({
+  const payload = destructPayload(values, req.payload);
+
+  reply(payload);
+
+  /* Conversation.create({
     type: req.payload.type,
   }).then(conversation => {
     reply({ data: conversation });
   }).catch(error => {
     reply({ message: error.message, errors: error.errors });
-  });
+  }); */
 };
