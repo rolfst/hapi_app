b590b30eccc55e00066c303252b3a1ded93e709f
Guido Schmitz <hello@guidoschmitz.co>
2016-03-18 12:41:46 +0100

    Cleaning

--- source/handlers/getMessages.js
+++ source/handlers/getMessages.js
@@ -2,25 +2,28 @@ import Boom from 'boom';
 import { Conversation, User } from 'models';
 import respondWithCollection from 'utils/respondWithCollection';
 import messageSerializer from 'serializers/message';
+import parseIncludes from 'utils/parseIncludes';
 
 module.exports = (req, reply) => {
+  const includes = parseIncludes(req.query);
+
   Conversation.findOne({
     where: { id: req.params.id },
   }).then(conversation => {
-    if (!conversation) return reply('Not found.');
+    if (!conversation) return reply(Boom.notFound('No conversation found for the given id.'));
 
-    return conversation.hasUser(req.auth.credentials.user).then(result => {
-      if (!result) return reply(Boom.forbidden('User doesn\'t belong to this conversation'));
+    if (!conversation.hasUser(req.auth.credentials.user)) {
+      return reply(Boom.forbidden('User doesn\'t belong to this conversation'));
+    }
 
-      return conversation.getMessages({
-        include: [{ model: User, attributes: ['id'] }],
-      }).then(messages => {
-        const response = respondWithCollection(messages, messageSerializer, {
-          relations: ['user'],
-        });
-
-        return reply(response);
-      });
+    return conversation.getMessages({
+      include: [{ model: User, attributes: ['id'] }],
     });
+  }).then(messages => {
+    const response = respondWithCollection(messages, messageSerializer, {
+      relations: ['user'],
+    });
+
+    return reply(response);
   });
 };
