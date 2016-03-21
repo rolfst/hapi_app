2448543f1a67a1a218c794084a73920102d8b786
Guido Schmitz <hello@guidoschmitz.co>
2016-03-21 15:20:03 +0100

    moved function to new line

--- source/handlers/getConversation.js
+++ source/handlers/getConversation.js
@@ -14,11 +14,12 @@ module.exports = (req, reply) => {
     modelIncludes.push({ model: Message });
   }
 
-  Conversation.findById(req.params.id, { include: modelIncludes }).then(conversation => {
-    if (!conversation.hasUser(req.auth.credentials.user)) {
-      throw Boom.forbidden('User doesn\'t belong to this conversation');
-    }
+  Conversation.findById(req.params.id, { include: modelIncludes })
+    .then(conversation => {
+      if (!conversation.hasUser(req.auth.credentials.user)) {
+        throw Boom.forbidden('User doesn\'t belong to this conversation');
+      }
 
-    return reply(respondWithItem(conversation, conversationSerializer));
-  }).catch(boom => reply(boom));
+      return reply(respondWithItem(conversation, conversationSerializer));
+    }).catch(boom => reply(boom));
 };
