d91a447966fd333950c948b9a1d6e8e3bb94896f
Guido Schmitz <hello@guidoschmitz.co>
2016-03-23 14:58:21 +0100

    added parse notifier service

---
+++ source/services/notifier.js
@@ -0,0 +1,5 @@
+import Parse from 'parse/node';
+
+Parse.initialize('JUBR9aZRJyKncIvD5ur2VnoTBCimM2kNd96iV4nK', 'y231ZoR9BcDeY3KPvqhew1g7J7PzfZb0IEjkzG8U', '7mKMwcrQyG1hrsfN0AcYYapLlwG0S9jPUCDooWSF');
+
+export default Parse;
