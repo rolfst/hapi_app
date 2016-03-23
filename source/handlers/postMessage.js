ea53ac4328a8952317d14ff3889d4b74b06fefda
Guido Schmitz <hello@guidoschmitz.co>
2016-03-23 11:17:13 +0100

    Extracted model creation to factory + cleaned some things

--- source/handlers/postMessage.js
+++ source/handlers/postMessage.js
@@ -1,5 +1,8 @@
 import Boom from 'boom';
+import fetch from 'isomorphic-fetch';
 import { Conversation, Message } from 'models';
+import messageFactory from 'factories/message';
+import socket from 'services/socket';
 import respondWithItem from 'utils/respondWithItem';
 import messageSerializer from 'serializers/message';
 
@@ -7,20 +10,17 @@ module.exports = (req, reply) => {
   Conversation.findById(req.params.id).then(conversation => {
     if (!conversation) throw Boom.notFound('No conversation found for id.');
 
-    // const message = messageFactory.buildForConversation(conversation.id, req.payload.body);
+    return messageFactory
+      .buildForConversation(conversation.id, req.auth.credentials.user.id, req.payload.text)
+      .save()
+      .then(createdMessage => Message.findById(createdMessage.id))
+      .then(message => {
+        socket.send('send-message', { message: message.text }, req.headers['x-api-token']);
 
-    return Message.create({
-      parentId: conversation.id,
-      parentType: 'FlexAppeal\\Entities\\Conversation',
-      text: req.payload.text,
-      createdBy: req.auth.credentials.user.id,
-      messageType: 'default',
-    }).then(createdMessage => {
-      return Message.findById(createdMessage.id);
-    }).then(createdMessage => {
-      return reply(respondWithItem(createdMessage, messageSerializer));
-    });
+        return reply(respondWithItem(message, messageSerializer));
+      });
   }).catch(error => {
+    console.error(error);
     reply(error);
   });
 };
