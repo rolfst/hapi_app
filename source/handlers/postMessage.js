702af902be6be7e3bcf5621dcd1d82e3476e36d6
Guido Schmitz <hello@guidoschmitz.co>
2016-03-23 15:14:55 +0100

    Implemented new web socket structure and send to every channel

--- source/handlers/postMessage.js
+++ source/handlers/postMessage.js
@@ -1,4 +1,5 @@
 import Boom from 'boom';
+import _ from 'lodash';
 import fetch from 'isomorphic-fetch';
 import { Conversation, Message } from 'models';
 import messageFactory from 'factories/message';
@@ -10,15 +11,18 @@ module.exports = (req, reply) => {
   Conversation.findById(req.params.id)
     .then(conversation => {
       if (!conversation) throw Boom.notFound('No conversation found for id.');
-
-      return messageFactory
+      const createdMessage = messageFactory
         .buildForConversation(conversation.id, req.auth.credentials.user.id, req.payload.text)
         .save();
-    })
-    .then(createdMessage => Message.findById(createdMessage.id))
-    .then(message => {
+
+      return [ createdMessage, conversation.getUsers() ];
+    }).spread((createdMessage, users) => {
+      return [ Message.findById(createdMessage.id), users ];
+    }).spread((message, users) => {
+      const userIds = _.map(users, 'id');
       const response = respondWithItem(message, messageSerializer);
-      socket.send('send-message', response, req.headers['x-api-token']);
+      
+      socket.send('send-message', userIds, response, req.headers['x-api-token']);
 
       return reply(response);
     }).catch(error => {
