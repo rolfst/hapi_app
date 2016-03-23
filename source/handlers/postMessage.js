73917fa91d6544c71ebd6944a9f1755b1bad9e3f
Guido Schmitz <hello@guidoschmitz.co>
2016-03-23 11:40:05 +0100

    Pass serialized message object to Websocket service

--- source/handlers/postMessage.js
+++ source/handlers/postMessage.js
@@ -7,20 +7,22 @@ import respondWithItem from 'utils/respondWithItem';
 import messageSerializer from 'serializers/message';
 
 module.exports = (req, reply) => {
-  Conversation.findById(req.params.id).then(conversation => {
-    if (!conversation) throw Boom.notFound('No conversation found for id.');
+  Conversation.findById(req.params.id)
+    .then(conversation => {
+      if (!conversation) throw Boom.notFound('No conversation found for id.');
 
-    return messageFactory
-      .buildForConversation(conversation.id, req.auth.credentials.user.id, req.payload.text)
-      .save()
-      .then(createdMessage => Message.findById(createdMessage.id))
-      .then(message => {
-        socket.send('send-message', { message: message.text }, req.headers['x-api-token']);
+      return messageFactory
+        .buildForConversation(conversation.id, req.auth.credentials.user.id, req.payload.text)
+        .save();
+    })
+    .then(createdMessage => Message.findById(createdMessage.id))
+    .then(message => {
+      const response = respondWithItem(message, messageSerializer);
+      socket.send('send-message', response, req.headers['x-api-token']);
 
-        return reply(respondWithItem(message, messageSerializer));
-      });
-  }).catch(error => {
-    console.error(error);
-    reply(error);
-  });
+      return reply(response);
+    }).catch(error => {
+      console.error(error);
+      reply(error);
+    });
 };
