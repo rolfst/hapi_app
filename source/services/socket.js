238b2ea98bd48466c19a8db0baf7e7633a44a979
Guido Schmitz <hello@guidoschmitz.co>
2016-03-24 12:14:54 +0100

    Fixed URL

--- source/services/socket.js
+++ source/services/socket.js
@@ -2,7 +2,7 @@ import fetch from 'isomorphic-fetch';
 
 export default {
   send: (eventName, userIds, payload, token) => {
-    fetch(`http://localhost:3000/${eventName}?token=${token}`, {
+    fetch(`http://realtime.flex-appeal.nl/${eventName}?token=${token}`, {
       method: 'POST',
       body: JSON.stringify({ payload, userIds }),
     });
