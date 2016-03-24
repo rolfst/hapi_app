7ebb049dadd1813cbf4d65530dfb8e947b7da67c
Guido Schmitz <hello@guidoschmitz.co>
2016-03-24 11:40:59 +0100

    Created notification service

--- source/handlers/postMessage.js
+++ source/handlers/postMessage.js
@@ -7,56 +7,29 @@ import respondWithItem from 'utils/respondWithItem';
 import messageSerializer from 'serializers/message';
 
 module.exports = (req, reply) => {
-  const Parse = notifier;
+  const loggedUser = req.auth.credentials.user;
 
-  // $payload = [
-  //           'where' => [
-  //               'loggedin_as_email' => [
-  //                   '$in' => $users->lists('email')
-  //               ]
-  //           ],
-  //           'push_time' => Carbon::now()->addMinute()->timestamp,
-  //           'data' => [
-  //               'alert' => $text,
-  //               'sound' => 'default',
-  //               'badge' => 'Increment',
-  //               'id' => $this->notification->getModel()->id,
-  //               'type' => $this->notification->getType()
-  //           ],
-  //       ];
-  const query = Parse.Query.containedIn('loggedin_as_email', ['ruben@flex-appeal.nl']);
-  console.log(query);
-  Parse.Push.send({
-    where: query,
-    push_time: moment().add(1, 'minutes').toISOString(),
-    data: {
-      alert: 'Test vanaf NodeJS',
-      sound: 'default',
-      badge: 'Increment',
-    },
-  }).then(function(object) {
-    console.log('success', object);
-  }).catch(err => console.error(err));
+  Conversation.findById(req.params.id)
+    .then(conversation => {
+      if (!conversation) throw Boom.notFound('No conversation found for id.');
+      const createdMessage = messageFactory
+        .buildForConversation(conversation.id, req.auth.credentials.user.id, req.payload.text)
+        .save();
 
-  // Conversation.findById(req.params.id)
-  //   .then(conversation => {
-  //     if (!conversation) throw Boom.notFound('No conversation found for id.');
-  //     const createdMessage = messageFactory
-  //       .buildForConversation(conversation.id, req.auth.credentials.user.id, req.payload.text)
-  //       .save();
-  //
-  //     return [createdMessage, conversation.getUsers()];
-  //   }).spread((createdMessage, users) => {
-  //     return [Message.findById(createdMessage.id), users];
-  //   }).spread((message, users) => {
-  //     const userIds = users.map(user => user.id);
-  //     const response = respondWithItem(message, messageSerializer);
-  //
-  //     socket.send('send-message', userIds, response, req.headers['x-api-token']);
-  //
-  //     return reply(response);
-  //   }).catch(error => {
-  //     console.error(error);
-  //     reply(error);
-  //   });
+      return [createdMessage, conversation.getUsers()];
+    }).spread((createdMessage, users) => {
+      return [Message.findById(createdMessage.id), users];
+    }).spread((message, users) => {
+      const ids = users.filter(user => user.id !== loggedUser.id).map(user => user.id);
+      const emails = users.filter(user => user.id !== loggedUser.id).map(user => user.email);
+      const response = respondWithItem(message, messageSerializer);
+
+      notifier.sendForMessage(message.id, emails, message.text);
+      socket.send('send-message', ids, response, req.headers['x-api-token']);
+
+      return reply(response);
+    }).catch(error => {
+      console.error(error);
+      reply(error);
+    });
 };
--- source/services/notifier.js
+++ source/services/notifier.js
@@ -1,5 +1,27 @@
 import Parse from 'parse/node';
+import moment from 'moment';
 
-Parse.initialize('JUBR9aZRJyKncIvD5ur2VnoTBCimM2kNd96iV4nK', 'y231ZoR9BcDeY3KPvqhew1g7J7PzfZb0IEjkzG8U', '7mKMwcrQyG1hrsfN0AcYYapLlwG0S9jPUCDooWSF');
+const sendPush = (emails, message, extraData) => {
+  Parse.initialize(process.env.PARSE_APP_ID, process.env.PARSE_CLIENT_KEY, process.env.PARSE_MASTER_KEY);
 
-export default Parse;
+  const query = new Parse.Query(Parse.Installation);
+  query.containedIn('loggedin_as_email', emails || []);
+
+  return Parse.Push.send({
+    where: query,
+    push_time: moment().add(1, 'minutes').toISOString(),
+    data: Object.assign({
+      alert: message,
+      sound: 'default',
+      badge: 'Increment',
+      network_id: null,
+    }, extraData),
+  }).catch(err => console.log('Parse error:', err));
+};
+
+export default {
+  sendPush,
+  sendForMessage: (messageId, emails, message, extraData) => {
+    return sendPush(emails, message, Object.assign({ id: messageId, type: 'message' }, extraData));
+  },
+};
