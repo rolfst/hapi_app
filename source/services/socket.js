84557ffba1906a55b7760bbed0f625ae56c31e92
Guido Schmitz <hello@guidoschmitz.co>
2016-03-24 13:45:35 +0100

    dynamic url as constant for environment

--- source/services/socket.js
+++ source/services/socket.js
@@ -1,8 +1,11 @@
 import fetch from 'isomorphic-fetch';
 
+export const WEBSOCKET_URL = process.env.NODE_ENV === 'development' ?
+  'http://localhost:3000' : 'http://realtime.flex-appeal.nl';
+
 export default {
   send: (eventName, userIds, payload, token) => {
-    fetch(`http://realtime.flex-appeal.nl/${eventName}?token=${token}`, {
+    fetch(`${WEBSOCKET_URL}/${eventName}?token=${token}`, {
       method: 'POST',
       body: JSON.stringify({ payload, userIds }),
     });
