fc671b29a5d88027e0fd4b2b9a3b9b9f0f5f123d
Guido Schmitz <hello@guidoschmitz.co>
2016-03-25 13:11:21 +0100

    started adding first test

--- tests/e2e/get-conversation.js
+++ tests/e2e/get-conversation.js
@@ -1,18 +1,55 @@
 import { assert } from 'chai';
-import User from 'models/User';
+import fetch from 'isomorphic-fetch';
+import Conversation from 'models/Conversation';
 
-// before(() => {
-//   return User.create({
-//     firstName: 'Test',
-//     lastName: 'Gebruiker',
-//     email: 'testgebruiker@flex-appeal.nl',
-//     profileImg: 'test.jpg',
-//     password: 'test',
-//   });
-// });
+import createServer from 'server';
+
+const server = createServer(8000);
+
+let token = null;
+let user = null;
+let conversation = null;
+
+const authenticate = () => {
+  return fetch('https://test.api.flex-appeal.nl/v1/authorize', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ email: 'ruben@flex-appeal.nl', password: 'admin' }),
+  }).then(response => response.json())
+    .then(json => json);
+}
+
+before(() => {
+  return authenticate().then(json => {
+    token = json.data.access_token;
+    user = json.data.user;
+
+    Conversation.create({
+      type: 'PRIVATE',
+      createdBy: user.id,
+    }).then(conversation => {
+      console.log('created', conversation);
+      conversation = conversation;
+    });
+  });
+});
 
 it('GET /conversations/:id', () => {
-  assert.equal(1, 1);
+  console.log(conversation);
+
+  return server.inject({
+    method: 'GET',
+    url: `/conversations/${conversation.id}`,
+    headers: {
+      'X-API-Token': token,
+    },
+    credentials: { user },
+  }).then(response => {
+    console.log(response.result);
+    assert.equal(response.statusCode, 200);
+  });
+
+  // assert.equal(1, 1);
 });
 
 // after(() => {
