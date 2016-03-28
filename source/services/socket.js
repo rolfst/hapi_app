b5956844f4f6e8e2605d31941dbaba36286cc12e
Liam <liam.hubers@gmail.com>
2016-03-28 23:37:50 +0200

    Changed payload to JSON

--- source/services/socket.js
+++ source/services/socket.js
@@ -1,13 +1,20 @@
 import fetch from 'isomorphic-fetch';
 
 export const WEBSOCKET_URL = process.env.NODE_ENV === 'development' ?
-  'http://localhost:3000' : 'http://realtime.flex-appeal.nl';
+  'http://localhost:2222' : 'http://realtime.flex-appeal.nl';
 
 export default {
   send: (eventName, userIds, payload, token) => {
+    const payloadJson = JSON.stringify(payload);
+
     fetch(`${WEBSOCKET_URL}/${eventName}?token=${token}`, {
       method: 'POST',
-      body: JSON.stringify({ payload, userIds }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ payload: payloadJson, userIds }),
+    }).then(res => {
+      console.log(res);
     });
   },
 };
