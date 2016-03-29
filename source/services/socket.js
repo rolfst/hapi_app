16604a237c87a9a9266eb0fcae8298b2b800c864
Guido Schmitz <guisch95@gmail.com>
2016-03-29 06:08:50 +0000

    Inline JSON stringify

--- source/services/socket.js
+++ source/services/socket.js
@@ -5,14 +5,12 @@ export const WEBSOCKET_URL = process.env.NODE_ENV === 'development' ?
 
 export default {
   send: (eventName, userIds, payload, token) => {
-    const payloadJson = JSON.stringify(payload);
-
     fetch(`${WEBSOCKET_URL}/${eventName}?token=${token}`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify({ payload: payloadJson, userIds }),
+      body: JSON.stringify({ payload: JSON.stringify(payload), userIds }),
     });
   },
 };
