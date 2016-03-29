61a3a5655061acf8ccf981b119c075f893034d7b
Liam <liam.hubers@gmail.com>
2016-03-29 09:24:12 +0200

    Fixed websocket url + added catch

--- source/services/socket.js
+++ source/services/socket.js
@@ -1,7 +1,6 @@
 import fetch from 'isomorphic-fetch';
 
-export const WEBSOCKET_URL = process.env.NODE_ENV === 'development' ?
-  'http://localhost:2222' : 'http://realtime.flex-appeal.nl';
+export const WEBSOCKET_URL = 'http://realtime.flex-appeal.nl';
 
 export default {
   send: (eventName, userIds, payload, token) => {
@@ -11,8 +10,8 @@ export default {
         'Content-Type': 'application/json',
       },
       body: JSON.stringify({ payload: JSON.stringify(payload), userIds }),
-    }).then(res => {
-      console.log(res);
+    }).catch(err => {
+      console.log(err);
     });
   },
 };
