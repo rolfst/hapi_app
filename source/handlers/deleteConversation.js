5a364417ad8e81bffa9b04d83a99c1c117318525
Liam <liam.hubers@gmail.com>
2016-04-06 15:37:54 +0200

    Fixed delete conversation call

--- source/handlers/deleteConversation.js
+++ source/handlers/deleteConversation.js
@@ -1,7 +1,12 @@
+import Boom from 'boom';
 import { Conversation } from 'models';
 
 module.exports = (req, reply) => {
   Conversation.findById(req.params.id).then(conversation => {
-    reply({ data: conversation });
+    return conversation.destroy();
+  }).then(() => {
+    reply('Successfully deleted conversation');
+  }).catch(err => {
+    reply(Boom.badRequest(err));
   });
 };
