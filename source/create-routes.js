d3a12483c1b4142857cdbee9f4bf9caf929e0d35
guidsen <guisch95@gmail.com>
2016-05-03 13:30:31 +0200

    boilerplate

--- source/create-routes.js
+++ source/create-routes.js
@@ -1,3 +1,24 @@
+import Boom from 'boom';
 import chatRoutes from 'modules/chat/routes/create-routes';
+import createAdapter from 'integrations/create-adapter';
 
-export default [...chatRoutes];
+const defaultRoutes = [
+  {
+    method: 'GET',
+    path: '/integrations/{name}/sync',
+    handler: (req, reply) => {
+      try {
+        const adapter = createAdapter(req.params.name).initialSync();
+
+        reply({ message: 'Succesfully synced integration.' }).code(202);
+      } catch (err) {
+        reply(Boom.badRequest(err));
+      }
+    },
+    config: {
+      auth: 'default',
+    },
+  },
+];
+
+export default [...defaultRoutes, ...chatRoutes];
