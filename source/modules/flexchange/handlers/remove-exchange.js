e773dee564e0c9e06c727d342cac29d88749e027
Liam Hubers <liam.hubers@gmail.com>
2016-05-18 11:55:32 +0200

    Added delete exchange handler

--- source/modules/flexchange/handlers/remove-exchange.js
+++ source/modules/flexchange/handlers/remove-exchange.js
@@ -1,3 +1,16 @@
+import { findNetworkById } from 'common/repositories/network';
+import { findExchangeById } from 'modules/flexchange/repositories/exchange';
+import hasIntegration from 'common/utils/network-has-integration';
+
 export default (req, reply) => {
-  reply('Not implemented yet.');
+  findNetworkById(req.params.networkId).then(network => {
+    if (hasIntegration(network)) {
+      // Execute integration logic with adapter
+    }
+
+    findExchangeById(req.params.exchangeId)
+      .then(exchange => exchange.destroy())
+      .then(() => reply('Successfully deleted exchange'))
+      .catch(err => reply(err));
+  });
 };
