fdc8b51775c5594db618992a9e3b32fcc2b09d12
Liam Hubers <liam.hubers@gmail.com>
2016-05-18 15:26:35 +0200

    Added jsdocs in flexchange module repositories

--- source/modules/flexchange/repositories/exchange.js
+++ source/modules/flexchange/repositories/exchange.js
@@ -2,6 +2,12 @@ import Boom from 'boom';
 import { Exchange } from 'modules/flexchange/models'; // eslint-disable-line
 import { User } from 'common/models';
 
+/**
+ * Find a specific exchange by id
+ * @param {number} exchangeId - Id of exchange being looked for
+ * @method findExchangeById
+ * @return {promise} Find exchange promise
+ */
 export function findExchangeById(exchangeId) {
   return Exchange
     .findById(exchangeId, { include: [{ model: User, as: 'ApprovedUser' }] })
@@ -12,23 +18,55 @@ export function findExchangeById(exchangeId) {
     });
 }
 
+/**
+ * Find exchanges by user
+ * @param {User} user - User we want the exchanges from
+ * @method findExchangesByUser
+ * @return {promise} Get exchanges promise
+ */
 export function findExchangesByUser(user) {
   return user.getExchanges();
 }
 
+/**
+ * Find exchange by network
+ * @param {Network} network - Netwerk we want the exchanges from
+ * @method findExchangesByNetwork
+ * @return {promise} Get exchanges promise
+ */
 export function findExchangesByNetwork(network) {
   return network.getExchanges();
 }
 
+/**
+ * Find exchanges by team
+ * @param {Team} team - Team we want the exchanges from
+ * @method findExchangesByTeam
+ * @return {promise} Get exchanges promise
+ */
 export function findExchangesByTeam(team) {
   return team.getExchanges();
 }
 
-export function deleteExchangeById(id) {
-  return findExchangeById(id)
+/**
+ * Delete a specific exchange by id
+ * @param {number} exchangeId - Id of exchange to be deleted
+ * @method deleteExchangeById
+ * @return {promise} Delete exchange promise
+ */
+export function deleteExchangeById(exchangeId) {
+  return findExchangeById(exchangeId)
     .then(exchange => exchange.destroy());
 }
 
+/**
+ * Create an new exchange
+ * @param {number} userId - Id of the user placing the exchange
+ * @param {number} networkId - Id of the network the exchange is being placed in
+ * @param {object} payload - Object containing payload data
+ * @method createExchange
+ * @return {promise} Create exchange promise
+ */
 export function createExchange(userId, networkId, payload) {
   const { title, description, date, type } = payload;
 
@@ -36,9 +74,16 @@ export function createExchange(userId, networkId, payload) {
     .create({ userId, networkId, title, description, date, type });
 }
 
-export function updateExchangeById(id, payload) {
+/**
+ * Update an existing exchange by id
+ * @param {number} exchangeId - Id of the exchange being updated
+ * @param {object} payload - Objecting containing payload data
+ * @method updateExchangeById
+ * @return {promise} Update exchange promise
+ */
+export function updateExchangeById(exchangeId, payload) {
   const { title, description } = payload;
 
-  return findExchangeById(id)
+  return findExchangeById(exchangeId)
     .then(exchange => exchange.update({ title, description }));
 }
