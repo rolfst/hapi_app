ae29a838570c977106ac28f58eaf8cd095eeb403
guidsen <guisch95@gmail.com>
2016-05-18 13:35:17 +0200

    removed console.log

--- source/modules/flexchange/handlers/view-exchange.js
+++ source/modules/flexchange/handlers/view-exchange.js
@@ -12,9 +12,6 @@ export default (req, reply) => {
 
     findExchangeById(req.params.exchangeId)
       .then(exchange => reply(respondWithItem(exchange)))
-      .catch(err => {
-        console.log(err);
-        reply(err)
-      });
+      .catch(err => reply(err));
   });
 };
