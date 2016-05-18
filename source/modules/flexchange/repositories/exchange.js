14cd4ceea2ed6f1e25d70b8414620c7e32b202dc
Liam Hubers <liam.hubers@gmail.com>
2016-05-18 10:48:03 +0200

    Removed duplicate function

--- source/modules/flexchange/repositories/exchange.js
+++ source/modules/flexchange/repositories/exchange.js
@@ -1,16 +1,6 @@
 import Boom from 'boom';
 import { Exchange } from 'modules/flexchange/models'; // eslint-disable-line
 
-export function findExchangeById(exchangeId) {
-  return Exchange
-    .findById(exchangeId)
-    .then(exchange => {
-      if (!exchange) return Boom.notFound(`No exchange found with id ${id}.`);
-
-      return exchange;
-    })
-}
-
 export function findExchangesByUser(user) {
   return user.getExchanges();
 }
