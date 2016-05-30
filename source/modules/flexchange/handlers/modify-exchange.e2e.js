ff1faf468bb37b747e6bfba78463e42bd8bd1295
guidsen <guisch95@gmail.com>
2016-05-30 11:24:44 +0200

    Added modify exchange e2e test case

---
+++ source/modules/flexchange/handlers/modify-exchange.e2e.js
@@ -0,0 +1,25 @@
+import { assert } from 'chai';
+import moment from 'moment';
+import { patchRequest } from 'common/test-utils/request';
+import { createExchange } from 'modules/flexchange/repositories/exchange';
+
+let exchange = null;
+
+describe('Modify exchange', () => {
+  before(() => {
+    return createExchange(global.authUser.id, global.network.id, {
+      date: moment().format('YYYY-MM-DD'),
+      type: 'ALL',
+      title: 'Test shift to check for modification',
+    }).then(createdExchange => (exchange = createdExchange));
+  });
+
+  it('should fail when action does not exist', () => {
+    const endpoint = `/v2/networks/${global.network.id}/exchanges/${exchange.id}`;
+
+    return patchRequest(endpoint, { action: 'action_that_does_not_exist' })
+      .then(response => {
+        assert.equal(response.statusCode, 422);
+      });
+  });
+});
