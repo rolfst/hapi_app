53abb5222545951276a921478cc59c04324a48fa
guidsen <guisch95@gmail.com>
2016-05-31 12:56:38 +0200

    Added view exchange e2e test

---
+++ source/modules/flexchange/handlers/view-exchange.e2e.js
@@ -0,0 +1,34 @@
+import { assert } from 'chai';
+import moment from 'moment';
+import { getRequest } from 'common/test-utils/request';
+import { createExchange } from 'modules/flexchange/repositories/exchange';
+
+let exchange = null;
+
+describe('View exchange', () => {
+  before(() => {
+    return createExchange(global.authUser.id, global.network.id, {
+      date: moment().format('YYYY-MM-DD'),
+      type: 'ALL',
+      title: 'Test shift to view',
+      description: 'Test description for this cool shift',
+    }).then(createdExchange => (exchange = createdExchange));
+  });
+
+  it('should return correct attributes', () => {
+    return getRequest(`/v2/networks/${global.network.id}/exchanges/${exchange.id}`)
+    .then(response => {
+      const { data } = response.result;
+
+      assert.equal(data.title, 'Test shift to view');
+      assert.equal(data.description, 'Test description for this cool shift');
+    });
+  });
+
+  it('should fail when exchange cannot be found', () => {
+    return getRequest(`/v2/networks/${global.network.id}/exchanges/${exchange.id + 1337}`)
+    .then(response => {
+      assert.equal(response.statusCode, 404);
+    });
+  });
+});
