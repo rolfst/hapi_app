b6c19d6abf7f66dd27464df72bd14eb4b9961c16
guidsen <guisch95@gmail.com>
2016-06-10 13:09:17 +0200

    Changed payload validation rules for username

--- source/modules/authentication/create-routes.js
+++ source/modules/authentication/create-routes.js
@@ -11,7 +11,7 @@ export default [
     config: {
       validate: {
         payload: {
-          username: Joi.string().alphanum().required(),
+          username: Joi.string().required(),
           password: Joi.string().required(),
         },
       },
