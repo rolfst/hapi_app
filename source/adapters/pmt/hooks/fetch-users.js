4baaf5fb1d13c1f230f9bac5ccede2b3d68bebee
guidsen <guisch95@gmail.com>
2016-06-17 09:47:04 +0200

    Set null value for the token parameter in PMT client

--- source/adapters/pmt/hooks/fetch-users.js
+++ source/adapters/pmt/hooks/fetch-users.js
@@ -1,7 +1,7 @@
 import client from 'adapters/pmt/client';
 
 export default (baseStoreUrl) => {
-  return client.get(`${baseStoreUrl}/users`)
+  return client.get(null, `${baseStoreUrl}/users`)
     .then(res => res.data)
     .catch(err => console.log(err));
 };
