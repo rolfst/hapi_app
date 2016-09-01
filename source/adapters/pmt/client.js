2ac288248fef31de60c3ffd577a9b79ee54f5757
guidsen <guisch95@gmail.com>
2016-09-01 20:52:43 +0200

    Added form-encoded url

--- source/adapters/pmt/client.js
+++ source/adapters/pmt/client.js
@@ -2,18 +2,28 @@ import fetch from 'isomorphic-fetch';
 import log from 'common/services/logger';
 import ExpiredToken from 'common/errors/token-expired';
 
+const createFormEncodedString = (data) => {
+  return Object.keys(data).map((key) => {
+    return `${encodeURIComponent(key)}=${encodeURIComponent(data[key])}`;
+  }).join('&');
+};
+
 export async function makeRequest(endpoint, token = null, method = 'GET', data = {}) {
   log.info('Calling PMT client', { url: endpoint, method, username: data.username });
 
   const response = await fetch(endpoint, {
     method,
     headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
       'api-key': 'testpmtapi',
     },
-    body: JSON.stringify(data),
+    body: createFormEncodedString(data),
   });
 
   const json = await response.json();
+  console.log('pmt json', json);
+
+  log.debug('PMT client responded with json', { json });
 
   if (response.status === 400) {
     log.info('PMT token expired', { username: data.username });
@@ -21,8 +31,6 @@ export async function makeRequest(endpoint, token = null, method = 'GET', data =
   } else if (!response.ok) {
     log.error('PMT Client error', { json: json.error || null, status_code: response.status });
   }
-
-  log.debug('PMT client responded with json', { json });
 
   return json;
 }
