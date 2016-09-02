608af5e7c937cd10f570e8b72f78532465819377
Guido Schmitz <guisch95@gmail.com>
2016-09-02 19:21:18 +0200

    Bugfix/invite user (#142)
    * Added test case for invite-user handler
    
    * Fixed invite-user bug

--- source/modules/employee/handlers/invite-user.js
+++ source/modules/employee/handlers/invite-user.js
@@ -1,10 +1,16 @@
+import camelCaseKeys from 'common/utils/camel-case-keys';
+import * as userRepo from 'common/repositories/user';
+import addNetworkScope from 'common/utils/add-network-scope';
 import inviteUser from 'modules/employee/controllers/invite-user';
 
 export default async (req, reply) => {
   try {
-    const invitedUser = await inviteUser(req.pre.network, req.payload);
+    const payload = camelCaseKeys(req.payload);
+    const network = req.pre.network;
+    await inviteUser(network, payload);
+    const invitedUser = await userRepo.findUserByEmail(req.payload.email);
 
-    return reply({ success: true, data: invitedUser.toJSON() });
+    return reply({ success: true, data: addNetworkScope(invitedUser, network.id).toJSON() });
   } catch (err) {
     return reply(err);
   }
