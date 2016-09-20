093d68272223c5354289d81d45c4265e4d68c2d4
guidsen <guisch95@gmail.com>
2016-09-20 15:57:10 +0200

    Fixed lint error

--- source/adapters/pmt/serializers/shift.js
+++ source/adapters/pmt/serializers/shift.js
@@ -1,10 +1,12 @@
 import * as dateUtils from '../../../shared/utils/date';
 
+const incomingDateFormat = 'DD-MM-YYYY HH:mm:ss';
+
 export default (externalShift) => ({
   id: externalShift.id,
-  date: dateUtils.getLocalDate(externalShift.start_time, 'DD-MM-YYYY HH:mm:ss').format('YYYY-MM-DD'),
-  start_time: dateUtils.getLocalDate(externalShift.start_time, 'DD-MM-YYYY HH:mm:ss').toISOString(),
-  end_time: dateUtils.getLocalDate(externalShift.end_time, 'DD-MM-YYYY HH:mm:ss').toISOString(),
+  date: dateUtils.getLocalDate(externalShift.start_time, incomingDateFormat).format('YYYY-MM-DD'),
+  start_time: dateUtils.getLocalDate(externalShift.start_time, incomingDateFormat).toISOString(),
+  end_time: dateUtils.getLocalDate(externalShift.end_time, incomingDateFormat).toISOString(),
   break: externalShift.break,
   team_id: externalShift.department,
 });
