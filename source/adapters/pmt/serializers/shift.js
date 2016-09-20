ca6d35d8007f3f0274d60f391cfaddf3c17f628b
guidsen <guisch95@gmail.com>
2016-09-20 16:49:59 +0200

    Remove debugging logs

--- source/adapters/pmt/serializers/shift.js
+++ source/adapters/pmt/serializers/shift.js
@@ -2,13 +2,11 @@ import moment from 'moment';
 
 const incomingDateFormat = 'DD-MM-YYYY HH:mm:ss';
 
-export default (externalShift) => {
-  return {
-    id: externalShift.id,
-    date: moment.utc(externalShift.start_time, incomingDateFormat).format('YYYY-MM-DD'),
-    start_time: moment.utc(externalShift.start_time, incomingDateFormat).toISOString(),
-    end_time: moment.utc(externalShift.end_time, incomingDateFormat).toISOString(),
-    break: externalShift.break,
-    team_id: externalShift.department,
-  };
-};
+export default (externalShift) => ({
+  id: externalShift.id,
+  date: moment.utc(externalShift.start_time, incomingDateFormat).format('YYYY-MM-DD'),
+  start_time: moment.utc(externalShift.start_time, incomingDateFormat).toISOString(),
+  end_time: moment.utc(externalShift.end_time, incomingDateFormat).toISOString(),
+  break: externalShift.break,
+  team_id: externalShift.department,
+});
