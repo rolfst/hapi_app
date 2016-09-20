7acd7e7504f64edabeaf3c21918561cccd2d942c
guidsen <guisch95@gmail.com>
2016-09-20 16:04:41 +0200

    Log UTC offset

--- source/adapters/pmt/hooks/view-shifts.e2e.js
+++ source/adapters/pmt/hooks/view-shifts.e2e.js
@@ -8,7 +8,7 @@ import hook from './view-shift';
 
 nock.disableNetConnect();
 
-describe('PMT view shifts hook', () => {
+describe.only('PMT view shifts hook', () => {
   const ENDPOINT = '/me/shifts';
   const TOKEN = 'aefacbadb0123456789';
   const TODAY = dateUtils.getLocalDate().format('DD-MM-YYYY');
--- source/adapters/pmt/serializers/shift.js
+++ source/adapters/pmt/serializers/shift.js
@@ -2,11 +2,15 @@ import * as dateUtils from '../../../shared/utils/date';
 
 const incomingDateFormat = 'DD-MM-YYYY HH:mm:ss';
 
-export default (externalShift) => ({
-  id: externalShift.id,
-  date: dateUtils.getLocalDate(externalShift.start_time, incomingDateFormat).format('YYYY-MM-DD'),
-  start_time: dateUtils.getLocalDate(externalShift.start_time, incomingDateFormat).toISOString(),
-  end_time: dateUtils.getLocalDate(externalShift.end_time, incomingDateFormat).toISOString(),
-  break: externalShift.break,
-  team_id: externalShift.department,
-});
+export default (externalShift) => {
+  console.log(dateUtils.getLocalDate(externalShift.start_time, incomingDateFormat).utcOffset());
+
+  return {
+    id: externalShift.id,
+    date: dateUtils.getLocalDate(externalShift.start_time, incomingDateFormat).format('YYYY-MM-DD'),
+    start_time: dateUtils.getLocalDate(externalShift.start_time, incomingDateFormat).toISOString(),
+    end_time: dateUtils.getLocalDate(externalShift.end_time, incomingDateFormat).toISOString(),
+    break: externalShift.break,
+    team_id: externalShift.department,
+  };
+};
