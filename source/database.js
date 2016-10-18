3643f0ab29cda9b31bb02c474c8bf97fd35d663b
guidsen <guisch95@gmail.com>
2016-10-18 15:26:25 +0200

    Added test db env vars

--- source/database.js
+++ source/database.js
@@ -2,10 +2,10 @@ require('dotenv').config();
 
 module.exports = {
   testing: {
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
+    host: process.env.TEST_DB_HOST,
+    database: process.env.TEST_DB_NAME,
+    username: process.env.TEST_DB_USERNAME,
+    password: process.env.TEST_DB_PASSWORD,
     dialect: 'mysql',
     migrationStorageTableName: 'sequelize_migrations',
   },
