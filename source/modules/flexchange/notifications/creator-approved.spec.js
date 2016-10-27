72b479f9b8e6565e2fb9896219fb32af5dcaacf7
Guido Schmitz <guisch95@gmail.com>
2016-10-27 13:47:12 +0200

    Bugfix/push notifications (#286)
    * Removed unused specific admin notification
    
    * Changed push notifications text and added extra test cases
    
    * Renamed route
    
    * Extracted duplicated logic to notification util

--- source/modules/flexchange/notifications/creator-approved.spec.js
+++ source/modules/flexchange/notifications/creator-approved.spec.js
@@ -3,27 +3,63 @@ import moment from 'moment';
 import { createNotification } from './creator-approved';
 
 describe('Your exchange approved notification', () => {
-  it('should return a correct notification object', () => {
-    const futureMoment = moment().add(2, 'weeks');
-    const exchange = { id: 1, date: futureMoment.format('YYYY-MM-DD') };
-    const approvedUser = { fullName: 'User#1' };
-    const actual = createNotification(exchange, approvedUser);
+  const createExchange = (baseMoment) => ({
+    id: 1,
+    date: baseMoment.format('YYYY-MM-DD'),
+    startTime: baseMoment.hour(10).minute(30).toISOString(),
+    endTime: baseMoment.hour(13).minute(0).toISOString(),
+    User: { id: 2, fullName: 'John Doe' },
+    ApprovedUser: { fullName: 'Pietje overnemer' },
+  });
 
+  it('should return a correct notification object for today', () => {
+    const futureMoment = moment();
+    const exchange = createExchange(futureMoment);
+
+    const actual = createNotification(exchange);
     const expected = {
-      text: `Er is een vervanger gevonden voor je shift op ${futureMoment.format('dddd DD MMMM')}`,
+      text: 'Pietje overnemer heeft je shift van vandaag van 10:30 tot 13:00 overgenomen.',
       data: { id: 1, type: 'exchange' },
     };
 
     assert.deepEqual(actual, expected);
   });
 
-  it('should return a correct notification object', () => {
-    const exchange = { id: 1, date: moment().add(1, 'days').format('YYYY-MM-DD') };
-    const approvedUser = { fullName: 'User#1' };
-    const actual = createNotification(exchange, approvedUser);
+  it('should return a correct notification object for tomorrow', () => {
+    const futureMoment = moment().add(1, 'days');
+    const exchange = createExchange(futureMoment);
 
+    const actual = createNotification(exchange);
     const expected = {
-      text: 'Er is een vervanger gevonden voor je shift voor morgen',
+      text: 'Pietje overnemer heeft je shift van morgen van 10:30 tot 13:00 overgenomen.',
+      data: { id: 1, type: 'exchange' },
+    };
+
+    assert.deepEqual(actual, expected);
+  });
+
+  it('should return a correct notification object for this week', () => {
+    const futureMoment = moment().add(2, 'days');
+    const exchange = createExchange(futureMoment);
+
+    const actual = createNotification(exchange);
+    const expected = {
+      text: `Pietje overnemer heeft je shift van ${futureMoment.format('dddd')} ` +
+        'van 10:30 tot 13:00 overgenomen.',
+      data: { id: 1, type: 'exchange' },
+    };
+
+    assert.deepEqual(actual, expected);
+  });
+
+  it('should return a correct notification object for later than this week', () => {
+    const futureMoment = moment().add(2, 'weeks');
+    const exchange = createExchange(futureMoment);
+
+    const actual = createNotification(exchange);
+    const expected = {
+      text: `Pietje overnemer heeft je shift op ${futureMoment.format('dddd DD MMMM')} ` +
+        'van 10:30 tot 13:00 overgenomen.',
       data: { id: 1, type: 'exchange' },
     };
 
