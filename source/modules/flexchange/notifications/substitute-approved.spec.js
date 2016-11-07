4be67e1c9911082f2416e730901256b3cb967873
guidsen <guisch95@gmail.com>
2016-11-07 15:09:28 +0100

    Transform timezone

--- source/modules/flexchange/notifications/substitute-approved.spec.js
+++ source/modules/flexchange/notifications/substitute-approved.spec.js
@@ -12,8 +12,8 @@ describe('Substitude flexchange notification', () => {
   });
 
   const localTime = {
-    startTime: moment().tz('Europe/Amsterdam').hour(10).minute(30).format('HH:mm'),
-    endTime: moment().tz('Europe/Amsterdam').hour(13).minute(0).format('HH:mm'),
+    startTime: moment(moment().hour(10).minute(30)).tz('Europe/Amsterdam').format('HH:mm'),
+    endTime: moment(moment().hour(13).minute(0)).tz('Europe/Amsterdam').format('HH:mm'),
   };
 
   it('should return a correct notification object for today', () => {
