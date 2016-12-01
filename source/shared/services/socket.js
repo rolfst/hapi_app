121d631d5aaaa574ec94a9b5fc8ca0ad374cc5a8
Guido Schmitz <guisch95@gmail.com>
2016-12-01 15:12:49 +0100

    Adding logging for the socket service (#311)

--- source/shared/services/socket.js
+++ source/shared/services/socket.js
@@ -1,5 +1,8 @@
 import fetch from 'isomorphic-fetch';
 import { map } from 'lodash';
+import * as Logger from './logger';
+
+const logger = Logger.getLogger('SHARED/services/socket');
 
 export const WEBSOCKET_URL = process.env.API_ENV === 'production' ?
   'https://realtime.flex-appeal.nl' : 'https://test.realtime.flex-appeal.nl';
@@ -8,6 +11,7 @@ export const send = (eventName, users, payload, token) => {
   if (process.env.API_ENV === 'testing') return;
 
   const userIds = map(users, 'id');
+  logger.info('Sending socket event', { userIds, eventName, payload });
 
   fetch(`${WEBSOCKET_URL}/${eventName}?token=${token}`, {
     method: 'POST',
