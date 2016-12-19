9a9fc024e9296b135b96507e717a6768841b4959
rolfst <rolfst@gmail.com>
2016-12-19 10:42:35 +0100

    feat(core-activity): added findAll and deleteById of activities

--- source/modules/core/repositories/activity.js
+++ source/modules/core/repositories/activity.js
@@ -51,3 +51,21 @@ export function findActivitiesForSource(sourceModel) {
 export function createActivity({ activityType, userId, sourceId, metaData }) {
   return Activity.create({ activityType, userId, sourceId, metaData });
 }
+
+/**
+ * Finds all Activities
+ * @method findAll
+ * @return {external:Promise.<Activity[]>} {@link module:shared~Activity Activity}
+ */
+export async function findAll() {
+  return Activity.findAll({ include: defaultIncludes });
+}
+
+/**
+ * Deletes all Activities
+ * @method deleteById
+ * @return {external:Promise.<number>} - number of deleted activities
+ */
+export async function deleteById(activityId) {
+  return Activity.destroy({ where: { id: activityId } })
+}
