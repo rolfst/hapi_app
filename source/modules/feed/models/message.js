d8fbf7610867f8bd54d3d724f1fa3f4539de744f
guidsen <guisch95@gmail.com>
2017-01-11 15:54:40 +0100

    Added missing property to message model

--- source/modules/feed/models/message.js
+++ source/modules/feed/models/message.js
@@ -1,6 +1,7 @@
 import * as dateUtils from '../../../shared/utils/date';
 
 export default (dataModel) => ({
+  type: 'message',
   id: dataModel.id.toString(),
   userId: dataModel.userId.toString(),
   text: dataModel.text,
