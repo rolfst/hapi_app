466738feb3cc9ed57c9dbe85a53f35da9704a6e2
guidsen <guisch95@gmail.com>
2017-01-12 18:48:10 +0100

    Renamed handlers + endpoints for feed

--- source/modules/feed/create-routes.js
+++ source/modules/feed/create-routes.js
@@ -2,19 +2,20 @@ import createRoutes from '../../shared/utils/create-routes';
 
 const routes = [{
   method: 'GET',
-  url: '/v2/networks/{networkId}/timeline',
-  handler: require('./handlers/get-network-timeline'),
+  url: '/v2/networks/{networkId}/feed',
+  handler: require('./handlers/get-network-feed'),
 }, {
   method: 'GET',
-  url: '/v2/teams/{teamId}/timeline',
-  handler: require('./handlers/get-team-timeline'),
+  url: '/v2/teams/{teamId}/feed',
+  handler: require('./handlers/get-team-feed'),
+  prefetch: false,
 }, {
   method: 'POST',
-  url: '/v2/networks/{networkId}/timeline',
+  url: '/v2/networks/{networkId}/feed',
   handler: require('./handlers/create-network-message'),
 }, {
   method: 'POST',
-  url: '/v2/teams/{teamId}/timeline',
+  url: '/v2/teams/{teamId}/feed',
   handler: require('./handlers/create-team-message'),
 }];
 
