8c8c7773557795a9fb882a2f58878e5335aa4413
guidsen <guisch95@gmail.com>
2017-01-17 11:30:07 +0100

    Added model properties

--- source/modules/flexchange/models/exchange.js
+++ source/modules/flexchange/models/exchange.js
@@ -3,12 +3,14 @@ import * as dateUtils from '../../../shared/utils/date';
 export default (dao) => ({
   type: 'exchange',
   id: dao.id.toString(),
+  userId: dao.userId.toString(),
   description: dao.description,
   date: dateUtils.toISOString(dao.date),
   startTime: dao.startTime ? dateUtils.toISOString(dao.startTime) : null,
   endTime: dao.endTime ? dateUtils.toISOString(dao.endTime) : null,
   acceptCount: dao.acceptCount,
   declineCount: dao.declineCount,
-  approvedUserId: !!dao.approvedUserId,
+  approvedById: dao.approvedBy ? dao.approvedBy.toString() : null,
+  approvedUserId: dao.approvedUserId ? dao.approvedUserId.toString() : null,
   createdAt: dateUtils.toISOString(dao.created_at),
 });
