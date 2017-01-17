eddd3724334cac6cdb97891f3466a0b8185d707b
guidsen <guisch95@gmail.com>
2017-01-17 11:30:14 +0100

    Added test data in before

--- source/modules/flexchange/services/flexchange/index.e2e.js
+++ source/modules/flexchange/services/flexchange/index.e2e.js
@@ -1,12 +1,30 @@
 import { assert } from 'chai';
-import exchangeService from './index';
+import moment from 'moment';
+import * as exchangeService from './index';
 
 describe.only('Service: Flexchange', () => {
   describe('list', () => {
+    let createdExchange1;
+
+    before(async () => {
+      createdExchange1 = await exchangeService.createExchange({
+        date: moment().toISOString(),
+        startTime: moment().toISOString(),
+        endTime: moment().add(2, 'hours').toISOString(),
+        type: 'ALL',
+        values: [global.networks.flexAppeal.id],
+      }, {
+        network: { id: global.networks.flexAppeal.id },
+        credentials: { id: global.users.admin.id },
+      });
+    });
+
     it('should return correct exchange models', async () => {
       const actual = await exchangeService.list({
-        networkId: '42',
-        exchangeIds: ['1', '2'],
+        networkId: global.networks.flexAppeal.id,
+        exchangeIds: [createdExchange1.id],
+      }, {
+        credentials: { id: global.users.admin.id },
       });
     });
   });
