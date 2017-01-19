39cb58c946115454cfd5c0f1ff218ba5598e5931
guidsen <guisch95@gmail.com>
2017-01-19 17:35:11 +0100

    Renamed files

---
+++ source/modules/chat/v1/handlers/get-messages.e2e.js
@@ -0,0 +1,53 @@
+import { assert } from 'chai';
+import { find } from 'lodash';
+import * as blueprints from '../../../../shared/test-utils/blueprints';
+import * as testHelper from '../../../../shared/test-utils/helpers';
+import { getRequest } from '../../../../shared/test-utils/request';
+import * as conversationRepo from '../repositories/conversation';
+import { createMessage } from '../repositories/message';
+
+describe.only('Handler: Get messages (v1)', () => {
+  let conversation;
+  let admin;
+  let employee;
+
+  before(async () => {
+    admin = await testHelper.createUser();
+    employee = await testHelper.createUser(blueprints.users.employee);
+    const network = await testHelper.createNetwork({ userId: admin.id });
+    const participants = [employee.id, admin.id];
+
+    await testHelper.addUserToNetwork({ networkId: network.id, userId: employee.id });
+    await testHelper.addUserToNetwork({
+      networkId: network.id, userId: admin.id, roleType: 'ADMIN' });
+
+    conversation = await conversationRepo.createConversation(
+      'PRIVATE', admin.id, participants);
+
+    return Promise.all([
+      createMessage(conversation.id, admin.id, 'Test bericht 1'),
+      createMessage(conversation.id, admin.id, 'Test bericht 2'),
+      createMessage(conversation.id, employee.id, 'Test bericht 3'),
+    ]);
+  });
+
+  after(() => {
+    Promise.all([
+      testHelper.deleteUser(employee),
+      testHelper.deleteUser(admin),
+    ]);
+  });
+
+  it('should return messages for conversation', async () => {
+    const endpoint = `/v1/chats/conversations/${conversation.id}/messages`;
+    const { tokens } = await testHelper.getLoginToken(blueprints.users.employee);
+
+    const { result, statusCode } = await getRequest(endpoint, tokens.access_token);
+    const actualMessage = find(result.data, { text: 'Test bericht 1' });
+
+    assert.equal(statusCode, 200);
+    assert.lengthOf(result.data, 3);
+    assert.isObject(actualMessage.created_by);
+    assert.equal(actualMessage.created_by.id, admin.id);
+  });
+});
