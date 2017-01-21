00bb959b09c38c9f5c63c0ae20a599780fb05bd6
guidsen <guisch95@gmail.com>
2017-01-21 13:08:58 +0100

    Change table name in migration

--- migrations/20170106211324-create_new_messages_table.js
+++ migrations/20170106211324-create_new_messages_table.js
@@ -2,36 +2,38 @@
 
 module.exports = {
   up: function (queryInterface, Sequelize) {
-    return queryInterface.createTable('messages', {
-      id: {
-        type: Sequelize.INTEGER.UNSIGNED,
-        primaryKey: true,
-        autoIncrement: true,
-      },
-      user_id: {
-        type: Sequelize.INTEGER.UNSIGNED,
-        allowNull: true,
-        references: {
-          model: 'users',
-          key: 'id',
+    queryInterface.renameTable('messages', 'old_messages').then(() =>
+      queryInterface.createTable('messages', {
+        id: {
+          type: Sequelize.INTEGER.UNSIGNED,
+          primaryKey: true,
+          autoIncrement: true,
         },
-        onDelete: 'set null',
-      },
-      text: {
-        type: Sequelize.STRING,
-        allowNull: false,
-      },
-      created_at: {
-        type: Sequelize.DATE,
-        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
-        allowNull: false,
-      },
-      updated_at: {
-        type: Sequelize.DATE,
-        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
-        allowNull: false,
-      },
-    });
+        user_id: {
+          type: Sequelize.INTEGER.UNSIGNED,
+          allowNull: true,
+          references: {
+            model: 'users',
+            key: 'id',
+          },
+          onDelete: 'set null',
+        },
+        text: {
+          type: Sequelize.STRING,
+          allowNull: false,
+        },
+        created_at: {
+          type: Sequelize.DATE,
+          defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+          allowNull: false,
+        },
+        updated_at: {
+          type: Sequelize.DATE,
+          defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+          allowNull: false,
+        },
+      })
+    );
   },
 
   down: function (queryInterface, Sequelize) {
