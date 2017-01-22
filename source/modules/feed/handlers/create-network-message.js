5dfee0e062dc0d7b861982c83f43290dc2080d95
guidsen <guisch95@gmail.com>
2017-01-22 15:19:14 +0100

    Call message service with correct attributes

--- source/modules/feed/handlers/create-network-message.js
+++ source/modules/feed/handlers/create-network-message.js
@@ -1,11 +1,11 @@
-import * as MessageService from '../services/message';
+import * as messageService from '../services/message';
 import * as responseUtil from '../../../shared/utils/response';
 
 export default async (req, reply) => {
   try {
-    const payload = { ...req.params, ...req.payload };
+    const payload = { parentType: 'network', parentId: req.params.networkId, ...req.payload };
     const message = { ...req.pre, ...req.auth };
-    const feedItems = await MessageService.createMessage(payload, message);
+    const feedItems = await messageService.create(payload, message);
 
     return reply({ data: responseUtil.toSnakeCase(feedItems) });
   } catch (err) {
--- source/modules/feed/handlers/create-team-message.js
+++ source/modules/feed/handlers/create-team-message.js
@@ -3,9 +3,9 @@ import * as responseUtil from '../../../shared/utils/response';
 
 export default async (req, reply) => {
   try {
-    const payload = { ...req.params, ...req.payload };
+    const payload = { parentType: 'team', parentId: req.params.teamId, ...req.payload };
     const message = { ...req.pre, ...req.auth };
-    const feedItems = await messageService.create(payload, message); // TODO
+    const feedItems = await messageService.create(payload, message);
 
     return reply({ data: responseUtil.toSnakeCase(feedItems) });
   } catch (err) {
