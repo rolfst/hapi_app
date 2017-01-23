c234600796aee8293c650700ddfb34cc3b675ab9
guidsen <guisch95@gmail.com>
2017-01-23 10:14:04 +0100

    Pass offset + limit to payload for repo

--- source/modules/feed/services/feed/index.js
+++ source/modules/feed/services/feed/index.js
@@ -15,6 +15,8 @@ const logger = Logger.getLogger('FEED/service/feed');
  * @param {object} payload - Object containing payload data
  * @param {string} payload.parentType - The type of parent to get objects for
  * @param {string} payload.parentId - The id of the parent
+ * @param {number} payload.limit - The limit of the resultset for pagination
+ * @param {number} payload.offset - The offset of the resultset for pagination
  * @param {Message} message {@link module:shared~Message message} - Object containing meta data
  * @method make
  * @return {external:Promise.<Object>} {@link module:modules/feed~Object}
@@ -22,8 +24,10 @@ const logger = Logger.getLogger('FEED/service/feed');
 export const make = async (payload, message) => {
   logger.info(`Making feed for ${payload.parentType}`, { payload, message });
 
-  const relatedObjects = await objectService.list(
-    R.pick(['parentType', 'parentId'], payload), message);
+  const whitelistedPayload = R.pick(['parentType', 'parentId', 'limit', 'offset'], payload);
+  const relatedObjects = await objectService
+    .list(whitelistedPayload, message)
+    .then(R.sort(R.descend(R.prop('createdAt'))));
 
   // Gathering the data to build the feed
   const objectSourceLinks = impl.createObjectSourceLinks(relatedObjects);
