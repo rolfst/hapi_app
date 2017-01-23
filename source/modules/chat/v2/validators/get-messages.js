f9143bfdd2797f17ee0db8dbf1b7dcbbfa31c006
guidsen <guisch95@gmail.com>
2017-01-23 16:47:02 +0100

    Changed default limit to 10

--- source/modules/chat/v2/validators/get-messages.js
+++ source/modules/chat/v2/validators/get-messages.js
@@ -3,7 +3,7 @@ import Joi from 'joi';
 export default {
   query: {
     include: Joi.any().valid(['participants']),
-    limit: Joi.number().min(0).max(50).default(20, 'The default limit'),
+    limit: Joi.number().min(0).max(50).default(10, 'The default limit'),
     offset: Joi.number().min(0).default(0, 'The default offset'),
   },
 };
