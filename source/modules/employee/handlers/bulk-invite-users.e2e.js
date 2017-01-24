5af1bc16392595c1c56b6603ece625caf18b429d
guidsen <guisch95@gmail.com>
2017-01-24 14:52:53 +0100

    Replaced global with test helper functions

--- source/modules/employee/handlers/bulk-invite-users.e2e.js
+++ source/modules/employee/handlers/bulk-invite-users.e2e.js
@@ -1,15 +1,13 @@
 import { assert } from 'chai';
 import Promise from 'bluebird';
-import { flatten, find, sortBy, partialRight, flow, map } from 'lodash';
+import sinon from 'sinon';
+import { flatten, sortBy, partialRight, flow, map } from 'lodash';
 import { postRequest } from '../../../shared/test-utils/request';
 import * as mailer from '../../../shared/services/mailer';
 import * as stubs from '../../../shared/test-utils/stubs';
 import { UserRoles } from '../../../shared/services/permission';
-import * as userRepo from '../../core/repositories/user';
-import * as integrationRepo from '../../core/repositories/integration';
-import * as networkRepo from '../../core/repositories/network';
-import * as networkService from '../../core/services/network';
 import * as userService from '../../core/services/user';
+import * as testHelpers from '../../../shared/test-utils/helpers';
 
 describe('Handler: Bulk invite users', () => {
   let createdUsers;
@@ -19,28 +17,56 @@ describe('Handler: Bulk invite users', () => {
   const pristineNetwork = stubs.pristine_network;
 
   // FIXME: Logic should be moved to service or repository
-  const importUser = async (user) => {
-    const networkId = user.networkId ? user.networkId : global.networks.pmt.id;
-    const roleType = user.isAdmin ? UserRoles.ADMIN : UserRoles.EMPLOYEE;
-    const employee = await userRepo.createUser({ ...user, password: 'fakepassword' });
+  const importUser = (networkId) => async (user) => {
+    const employee = await testHelpers.createUser({ ...user, password: 'fakepassword' });
 
-    await networkRepo.addUser({
-      userId: employee.id, networkId, roleType });
+    await testHelpers.addUserToNetwork({
+      networkId,
+      userId: employee.id,
+      roleType: user.isAdmin ? UserRoles.ADMIN : UserRoles.EMPLOYEE,
+    });
 
     return employee;
   };
 
+  let adminToken;
+  let employee;
+  let employeeToken;
+  let network;
+
   before(async () => {
-    createdUsers = sortImportedUsers(await Promise.map(toBeImportedUsers, importUser));
+    sinon.stub(mailer, 'send');
+
+    const admin = await testHelpers.createUser({
+      username: 'admin@flex-appeal.nl', password: 'foo' });
+    employee = await testHelpers.createUser({
+      username: 'employee@flex-appeal.nl', password: 'baz' });
+
+    network = await testHelpers.createNetwork({ userId: admin.id });
+
+    [adminToken, employeeToken] = await Promise.all([
+      testHelpers.getLoginToken({ username: admin.username, password: 'foo' }),
+      testHelpers.getLoginToken({ username: employee.username, password: 'baz' }),
+    ]);
+
+    await Promise.all([
+      testHelpers.addUserToNetwork({
+        userId: employee.id, networkId: network.id, roleType: 'EMPLOYEE' }),
+      testHelpers.addUserToNetwork({
+        userId: admin.id, networkId: network.id, roleType: 'ADMIN' }),
+    ]);
+
+    createdUsers = sortImportedUsers(await Promise.map(toBeImportedUsers, importUser(network.id)));
   });
 
-  after(() => Promise.map(map(createdUsers, 'id'), userRepo.deleteById));
+  after(() => testHelpers.cleanAll());
 
   describe('Happy path', () => {
     it('should add to the network as admin', async () => {
       const userIds = map(createdUsers, 'id');
-      const endpoint = `/v2/networks/${global.networks.pmt.id}/users/invite`;
-      const { statusCode } = await postRequest(endpoint, { user_ids: userIds });
+      const endpoint = `/v2/networks/${network.id}/users/invite`;
+      const { statusCode } = await postRequest(
+        endpoint, { user_ids: userIds }, adminToken.tokens.access_token);
 
       assert.equal(statusCode, 200);
       assert.equal(mailer.send.called, true);
@@ -48,47 +74,46 @@ describe('Handler: Bulk invite users', () => {
 
     it('should set invited_at value', async () => {
       const userIds = map(createdUsers, 'id');
-      const endpoint = `/v2/networks/${global.networks.pmt.id}/users/invite`;
-      const { statusCode } = await postRequest(endpoint, { user_ids: userIds });
+      const endpoint = `/v2/networks/${network.id}/users/invite`;
+      const { statusCode } = await postRequest(
+        endpoint, { user_ids: userIds }, adminToken.tokens.access_token);
       const invitedUser = await userService.getUserWithNetworkScope({
-        id: userIds[0], networkId: global.networks.pmt.id });
+        id: userIds[0], networkId: network.id });
 
       assert.equal(statusCode, 200);
       assert.isTrue(invitedUser.invitedAt !== null);
     });
   });
 
-  describe('Faulty path', () => {
-    let integration;
-    let network;
+  describe.only('Faulty path', () => {
+    let alternativeNetwork;
+    let alternativeAdminToken;
 
     before(async () => {
-      const adminUser = find(createdUsers, { username: stubs.pristine_admin.username });
+      const alternativeAdmin = await testHelpers.createUser({
+        username: 'alternativeAdmin@flex-appeal.nl', password: 'foo' });
 
-      integration = await integrationRepo.createIntegration({
-        name: pristineNetwork.integrationName,
+      alternativeAdminToken = await testHelpers.getLoginToken({
+        username: alternativeAdmin.username, password: 'foo' });
+
+      alternativeNetwork = await testHelpers.createNetworkWithIntegration({
+        userId: alternativeAdmin.id,
+        name: pristineNetwork.name,
+        externalId: pristineNetwork.networkId,
+        integrationName: pristineNetwork.integrationName,
         token: 'footoken',
       });
-
-      network = await networkRepo.createNetwork(
-        adminUser.id, pristineNetwork.name, pristineNetwork.networkId);
-    });
-
-    after(async () => {
-      return Promise.all([
-        integration.destroy(),
-        networkRepo.deleteById(network.id),
-      ]);
     });
 
     it('should only process user ids that belong to the network', async () => {
       const userIds = map(createdUsers, 'id');
-      const endpoint = `/v2/networks/${global.networks.pmt.id}/users/invite`;
-      const { statusCode } = await postRequest(endpoint, { user_ids: userIds });
+      const endpoint = `/v2/networks/${network.id}/users/invite`;
+      const { statusCode } = await postRequest(
+        endpoint, { user_ids: userIds }, adminToken.tokens.access_token);
       const invitedUser = await userService.getUserWithNetworkScope({
-        id: userIds[0], networkId: global.networks.pmt.id });
+        id: userIds[0], networkId: network.id });
       const unlinkedUser = await userService.getUserWithNetworkScope({
-        id: global.users.employee.id, networkId: global.networks.flexAppeal.id });
+        id: employee.id, networkId: alternativeNetwork.id });
 
       assert.equal(statusCode, 200);
       assert.isTrue(invitedUser.invitedAt !== null);
@@ -96,23 +121,23 @@ describe('Handler: Bulk invite users', () => {
     });
 
     it('should return 403 when authenticated user is a regular user', async () => {
-      await networkService.addUserToNetwork({
+      await testHelpers.addUserToNetwork({
         networkId: network.id,
-        userId: global.users.employee.id,
+        userId: employee.id,
         roleType: UserRoles.EMPLOYEE,
       });
 
       const endpoint = `/v2/networks/${network.id}/users/invite`;
       const userIds = map(createdUsers, 'id');
       const { statusCode } = await postRequest(endpoint, {
-        user_ids: userIds }, global.server, global.users.employee.token);
+        user_ids: userIds }, employeeToken.tokens.access_token);
 
       assert.equal(statusCode, 403);
     });
 
     it('should return 422 because of missing user_ids payload', async () => {
       const endpoint = `/v2/networks/${network.id}/users/invite`;
-      const { statusCode } = await postRequest(endpoint);
+      const { statusCode } = await postRequest(endpoint, {}, adminToken.tokens.access_token);
 
       assert.equal(statusCode, 422);
     });
@@ -120,7 +145,8 @@ describe('Handler: Bulk invite users', () => {
     it('should fail because of bad network access', async () => {
       const endpoint = `/v2/networks/${network.id}/users/invite`;
       const userIds = map(createdUsers, 'id');
-      const { statusCode } = await postRequest(endpoint, { user_ids: userIds });
+      const { statusCode } = await postRequest(
+        endpoint, { user_ids: userIds }, alternativeAdminToken.tokens.access_token);
 
       assert.equal(statusCode, 403);
     });
