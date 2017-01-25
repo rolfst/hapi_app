86ebd3a872c651ce1c058e2c63cbf532b719caaf
guidsen <guisch95@gmail.com>
2017-01-25 14:28:15 +0100

    Removed unused code

--- source/modules/core/services/user/implementation.js
+++ source/modules/core/services/user/implementation.js
@@ -1,15 +1,3 @@
-import { first, map } from 'lodash';
-import * as teamRepo from '../../repositories/team';
-
-export const createFunctionName = async (userId, network) => {
-  const teamsThatUserBelongsTo = await teamRepo
-    .findTeamsForNetworkThatUserBelongsTo(userId, network.id);
-
-  if (teamsThatUserBelongsTo.length === 0) return network.name;
-
-  return first(map(teamsThatUserBelongsTo, 'name'));
-};
-
 export async function createScopedUser(user, metaData) {
   return {
     ...user,
