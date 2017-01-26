6d822bb2b6da2f39ef9f35670b0ffbe566094bad
guidsen <guisch95@gmail.com>
2017-01-26 15:02:58 +0100

    Changed imports to new path

--- source/modules/feed/repositories/message.js
+++ source/modules/feed/repositories/message.js
@@ -1,32 +1,32 @@
 import R from 'ramda';
-import { Message } from './dao';
-import createDomainObject from '../models/message';
+import { FeedMessage } from './dao';
+import createFeedMessageModel from '../models/feed-message';
 
 /**
  * Find a message by id
  * @param {string} messageId - The id of the message
  * @method findById
- * @return {external:Promise.<Message>} {@link module:modules/feed~Message}
+ * @return {external:Promise.<FeedMessage>} {@link module:modules/feed~FeedMessage}
  */
 export const findById = async (messageId) => {
-  const result = await Message.findById(messageId);
+  const result = await FeedMessage.findById(messageId);
   if (!result) return null;
 
-  return createDomainObject(result);
+  return createFeedMessageModel(result);
 };
 
 /**
  * Find messages by ids
  * @param {string[]} messageIds - The ids of the messages
  * @method findByIds
- * @return {external:Promise.<Message[]>} {@link module:modules/feed~Message}
+ * @return {external:Promise.<FeedMessage[]>} {@link module:modules/feed~FeedMessage}
  */
 export const findByIds = async (messageIds) => {
-  const results = await Message.findAll({
+  const results = await FeedMessage.findAll({
     where: { id: { in: messageIds } },
   });
 
-  return R.map(createDomainObject, results);
+  return R.map(createFeedMessageModel, results);
 };
 
 /**
@@ -35,17 +35,17 @@ export const findByIds = async (messageIds) => {
  * @param {string} attributes.userId - The user id that creates the message
  * @param {string} attributes.text - The text that contains the message
  * @method create
- * @return {external:Promise.<Message>} {@link module:modules/feed~Message}
+ * @return {external:Promise.<FeedMessage>} {@link module:modules/feed~FeedMessage}
  */
 export const create = async (attributes) => {
   const attributesWhitelist = ['userId', 'text'];
-  const result = await Message.create(R.pick(attributesWhitelist, attributes));
+  const result = await FeedMessage.create(R.pick(attributesWhitelist, attributes));
 
-  return createDomainObject(result);
+  return createFeedMessageModel(result);
 };
 
-export const update = async (messageId, attributes) => Message.update(attributes, {
+export const update = async (messageId, attributes) => FeedMessage.update(attributes, {
   where: { id: messageId },
 });
 
-export const destroy = (messageId) => Message.destroy({ where: { id: messageId } });
+export const destroy = (messageId) => FeedMessage.destroy({ where: { id: messageId } });
