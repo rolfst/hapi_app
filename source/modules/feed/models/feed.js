4141ef2ea456cbc90e41d59638702e410e319fda
guidsen <guisch95@gmail.com>
2017-01-27 10:00:25 +0100

    Removed unused model

--- source/modules/feed/models/feed.js
+++
@@ -1,9 +0,0 @@
-export default (objectModel) => ({
-  objectId: objectModel.id,
-  objectType: objectModel.objectType,
-  parentType: objectModel.parentType,
-  parentId: objectModel.parentId,
-  userId: objectModel.userId,
-  source: objectModel.source || {},
-  createdAt: objectModel.createdAt,
-});
