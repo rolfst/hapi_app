b1b74b5400e1d948b5e32f57024932a879f1fbf8
guidsen <guisch95@gmail.com>
2017-01-30 11:29:56 +0100

    Added DAO to index.js

--- source/modules/feed/repositories/dao/index.js
+++ source/modules/feed/repositories/dao/index.js
@@ -1,7 +1,11 @@
-import FeedMessageModel from './feed-message';
+import FeedMessageModel from './message';
 import ObjectModel from './object';
+import LikeModel from './like';
+import CommentModel from './comment';
 
 module.exports = {
   FeedMessage: FeedMessageModel,
   _Object: ObjectModel,
+  Like: LikeModel,
+  Comment: CommentModel,
 };
