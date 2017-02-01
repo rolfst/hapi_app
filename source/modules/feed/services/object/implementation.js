2b6536f352e4499a2e75528a6abaf8b301df0f4c
guidsen <guisch95@gmail.com>
2017-02-01 17:37:59 +0100

    Added support for poll as child in object model

--- source/modules/feed/services/object/implementation.js
+++ source/modules/feed/services/object/implementation.js
@@ -1,7 +1,12 @@
 import R from 'ramda';
 import * as flexchangeService from '../../../flexchange/services/flexchange';
+import * as pollService from '../../../poll/services/poll';
 import * as privateMessageService from '../../../chat/v2/services/private-message';
+import * as objectRepository from '../../repositories/object';
 import * as feedMessageService from '../message';
+
+const whereTypeAndId = (type, id) => R.both(R.propEq('type', type), R.propEq('id', id));
+const findSource = (type, id, sources) => R.find(whereTypeAndId(type, id), sources) || null;
 
 /**
  * Find the sources for the object by type.
@@ -15,4 +20,13 @@ export const findSourcesForType = R.curry((message, values, type) => R.cond([
   [R.equals('private_message'), () => privateMessageService.list({ messageIds: values }, message)],
   [R.equals('feed_message'), () => feedMessageService.list({ messageIds: values }, message)],
   [R.equals('exchange'), () => flexchangeService.list({ exchangeIds: values }, message)],
+  [R.equals('poll'), () => pollService.list({ pollIds: values }, message)],
 ])(type, values));
+
+export const findChildrenForType = R.curry((values, type) => R.cond([
+  [R.equals('feed_message'), () => objectRepository.findBy({
+    parentType: 'feed_message', parentId: { $in: values } })],
+])(type, values));
+
+export const addSourceToObject = R.curry((sources, object) =>
+  R.merge(object, { source: findSource(object.objectType, object.sourceId, sources) }));
