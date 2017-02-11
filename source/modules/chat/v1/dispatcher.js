27b16acf0937d77fb543d1a625b0863696c55aa3
guidsen <guisch95@gmail.com>
2017-02-11 14:19:01 +0100

    Send socket first to avoid errors

--- source/modules/chat/v1/dispatcher.js
+++ source/modules/chat/v1/dispatcher.js
@@ -8,11 +8,10 @@ const pubsub = EventEmitter.create();
 pubsub.on('message.created', async (payload) => {
   const usersToNotify = payload.conversation.users.filter(
     user => user.id !== payload.message.createdBy.id);
-
-  newMessageNotification.send(payload.message, usersToNotify);
-
   const socketPayload = { data: responseUtils.toSnakeCase(payload.message) };
+
   socketService.send('send-message', usersToNotify, socketPayload, payload.token);
+  newMessageNotification.send(payload.message, usersToNotify);
 });
 
 export default pubsub;
