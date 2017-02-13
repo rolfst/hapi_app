1cb00714e795dfc5e7ad1c1d0b3b2f37470f6147
guidsen <guisch95@gmail.com>
2017-02-13 14:32:47 +0100

    Created and remove objects for specific exchange actions

--- source/modules/flexchange/dispatcher.js
+++ source/modules/flexchange/dispatcher.js
@@ -4,6 +4,7 @@ import EventEmitter from '../../shared/services/event-emitter';
 import * as Intercom from '../../shared/services/intercom';
 import approveExchangeEvent from '../../shared/events/approve-exchange-event';
 import newExchangeEvent from '../../shared/events/new-exchange-event';
+import * as objectService from '../feed/services/object';
 import * as creatorNotifier from './notifications/creator-approved';
 import * as substituteNotifier from './notifications/substitute-approved';
 import * as createdNotifier from './notifications/exchange-created';
@@ -12,13 +13,27 @@ import * as exchangeService from './services/flexchange';
 const pubsub = EventEmitter.create();
 
 pubsub.on('exchange.created', async (payload) => {
-  const { exchange, network, exchangeValues, credentials } = payload;
+  const { exchange, network, credentials } = payload;
   const intercomEventPayload = R.pick([
     'networkId', 'date', 'startTime', 'endTime', 'type'], exchange);
-  const usersToNotify = await exchangeService.findUsersByType(
-    exchange.type, network.id, exchangeValues, credentials.id);
+  const exchangeReceivers = await exchangeService.listReceivers({
+    exchangeId: exchange.id,
+  }, {
+    credentials,
+    network,
+  });
 
-  createdNotifier.send(usersToNotify, payload.exchange);
+  const promisedObjects = R.map((receiver) => objectService.create({
+    userId: receiver.id,
+    parentType: 'user',
+    parentId: receiver.id,
+    objectType: 'exchange',
+    sourceId: exchange.id,
+  }), exchangeReceivers);
+
+  Promise.all(promisedObjects);
+
+  createdNotifier.send(exchangeReceivers, payload.exchange);
   Analytics.track(newExchangeEvent(network.id, exchange), credentials.id);
   Intercom.createEvent(credentials.username, 'exchange.create', intercomEventPayload);
   Intercom.incrementAttribute(credentials.username, 'created_shifts');
@@ -27,6 +42,7 @@ pubsub.on('exchange.created', async (payload) => {
 pubsub.asyncOn('exchange.approved', async (payload) => {
   const { exchange, network, credentials, approvedUser } = payload;
 
+  objectService.remove({ objectType: 'exchange', sourceId: exchange.id });
   creatorNotifier.send(exchange);
   substituteNotifier.send(exchange);
   Analytics.track(approveExchangeEvent(network, payload.exchange), credentials.id);
