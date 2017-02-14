d099924f984a123d7f375294157f94e47831765f
guidsen <guisch95@gmail.com>
2017-02-14 12:11:37 +0100

    Improved logging

--- source/shared/services/socket.js
+++ source/shared/services/socket.js
@@ -4,14 +4,13 @@ import * as Logger from './logger';
 
 const logger = Logger.createLogger('SHARED/services/socket');
 
-export const WEBSOCKET_URL = process.env.API_ENV === 'production' ?
-  'https://realtime.flex-appeal.nl' : 'https://test.realtime.flex-appeal.nl';
+// export const WEBSOCKET_URL = process.env.API_ENV === 'production' ?
+//   'https://realtime.flex-appeal.nl' : 'https://test.realtime.flex-appeal.nl';
+export const WEBSOCKET_URL = 'https://realtime.flex-appeal.nl';
 
 export const send = (eventName, users, payload, token) => {
-  if (process.env.API_ENV === 'testing') return;
-
   const userIds = R.pluck('id', users);
-  logger.info('Sending socket event', { userIds, eventName, payload });
+  logger.info('Sending socket event', { userIds, eventName, payload, token });
 
   fetch(`${WEBSOCKET_URL}/${eventName}?token=${token}`, {
     method: 'POST',
