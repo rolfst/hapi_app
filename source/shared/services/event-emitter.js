cf061fd882ffb0f556df79d25b08728ab240bae8
guidsen <guisch95@gmail.com>
2017-02-14 12:07:23 +0100

    Added better error handling for event emitter

--- source/shared/services/event-emitter.js
+++ source/shared/services/event-emitter.js
@@ -1,13 +1,23 @@
 import NativeEventEmitter from 'events';
+import * as Logger from './logger';
 
 class EventEmitter extends NativeEventEmitter {
   constructor() {
     super();
 
-    this.asyncOn = function (eventName, fn) { // eslint-disable-line func-names
-      return this.on(eventName, (...args) =>
-        setImmediate(() => fn.apply(this, args)));
-    };
+    this.logger = Logger.createLogger('SHARED/eventEmitter');
+  }
+
+  setLogger(logger) {
+    this.logger = logger;
+  }
+
+  asyncOn(eventName, fn) {
+    const that = this;
+    return this.on(eventName, (...args) => {
+      return fn.apply(this, args).catch(err =>
+        that.logger.error('Error while emitter event', { eventName, err }));
+    });
   }
 }
 
