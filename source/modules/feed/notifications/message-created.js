5679b3a1c76b1061ababd22ea68a33af73217d77
guidsen <guisch95@gmail.com>
2017-02-15 13:46:50 +0100

    Changed message created notification signature

--- source/modules/feed/notifications/message-created.js
+++ source/modules/feed/notifications/message-created.js
@@ -1,10 +1,10 @@
-export default (actor, parent, message) => {
+export default (actor, parent, object) => {
   const text = (!!parent.name) ?
-    `${actor.fullName} in ${parent.name}: ${message.text}` :
-    `${actor.fullName}: ${message.text}`;
+    `${actor.fullName} in ${parent.name}: ${object.source.text}` :
+    `${actor.fullName}: ${object.source.text}`;
 
   return {
     text,
-    data: { id: message.id, type: 'message', track_name: 'message_created' },
+    data: { id: object.source.id, type: 'message', track_name: 'message_created' },
   };
 };
