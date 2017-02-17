b0034464b1997ba44ea364760e72955bb2b138d0
guidsen <guisch95@gmail.com>
2017-02-17 12:43:57 +0100

    Export storage function to get location

--- source/modules/attachment/models/attachment.js
+++ source/modules/attachment/models/attachment.js
@@ -1,9 +1,10 @@
 import * as dateUtils from '../../../shared/utils/date';
+import * as Storage from '../../../shared/services/storage';
 
 export default (dao) => ({
   type: 'attachment',
   id: dao.id.toString(),
   objectId: dao.objectId ? dao.objectId.toString() : null,
-  path: dao.path,
+  path: `https://assets.flex-appeal.nl/${Storage.getLocation()}/attachments/${dao.path}`,
   createdAt: dateUtils.toISOString(dao.created_at),
 });
