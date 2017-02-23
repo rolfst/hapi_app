b2fb1b2251854e418a23f05892eaadff37c31126
guidsen <guisch95@gmail.com>
2017-02-23 16:08:49 +0100

    Fixed SSL issue on testing environment

--- source/shared/services/socket.js
+++ source/shared/services/socket.js
@@ -10,21 +10,25 @@ export const WEBSOCKET_URL = process.env.API_ENV === 'production' ?
 export const send = async (eventName, users, payload, token) => {
   const userIds = R.pluck('id', users);
 
-  const response = await fetch(`${WEBSOCKET_URL}/${eventName}?token=${token}`, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({ payload: JSON.stringify(payload), userIds }),
-  });
+  try {
+    const response = await fetch(`${WEBSOCKET_URL}/${eventName}?token=${token}`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ payload: JSON.stringify(payload), userIds }),
+    });
 
-  const jsonResponse = await response.json();
+    const jsonResponse = await response.json();
 
-  if (response.ok) {
-    logger.info('Successfully send socket event', {
-      userIds, eventName, payload, token, jsonResponse });
-  } else {
-    logger.error('Error sending socket event', {
-      userIds, eventName, payload, jsonResponse });
+    if (response.ok) {
+      logger.info('Successfully send socket event', {
+        userIds, eventName, payload, token, jsonResponse });
+    } else {
+      logger.error('Error sending socket event', {
+        userIds, eventName, payload, jsonResponse });
+    }
+  } catch (err) {
+    logger.error('Error sending socket event', { err });
   }
 };
