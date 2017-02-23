9db1faa79619e66e1f4b81612c9446dc998be601
guidsen <guisch95@gmail.com>
2017-02-23 15:02:21 +0100

    Added logging for websocket api

--- source/shared/services/socket.js
+++ source/shared/services/socket.js
@@ -7,15 +7,24 @@ const logger = Logger.createLogger('SHARED/services/socket');
 export const WEBSOCKET_URL = process.env.API_ENV === 'production' ?
   'https://realtime.flex-appeal.nl' : 'https://test.realtime.flex-appeal.nl';
 
-export const send = (eventName, users, payload, token) => {
+export const send = async (eventName, users, payload, token) => {
   const userIds = R.pluck('id', users);
-  logger.info('Sending socket event', { userIds, eventName, payload, token });
 
-  fetch(`${WEBSOCKET_URL}/${eventName}?token=${token}`, {
+  const response = await fetch(`${WEBSOCKET_URL}/${eventName}?token=${token}`, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
     },
     body: JSON.stringify({ payload: JSON.stringify(payload), userIds }),
   });
+
+  const jsonResponse = await response.json();
+
+  if (response.ok) {
+    logger.info('Successfully send socket event', {
+      userIds, eventName, payload, token, jsonResponse });
+  } else {
+    logger.error('Error sending socket event', {
+      userIds, eventName, payload, jsonResponse });
+  }
 };
