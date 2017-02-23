7ef0fb937cbdb5bc40db295cfd6038107abdc7d5
guidsen <guisch95@gmail.com>
2017-02-23 14:38:22 +0100

    Fixed websocket URL

--- source/shared/services/socket.js
+++ source/shared/services/socket.js
@@ -4,9 +4,8 @@ import * as Logger from './logger';
 
 const logger = Logger.createLogger('SHARED/services/socket');
 
-// export const WEBSOCKET_URL = process.env.API_ENV === 'production' ?
-//   'https://realtime.flex-appeal.nl' : 'https://test.realtime.flex-appeal.nl';
-export const WEBSOCKET_URL = 'https://realtime.flex-appeal.nl';
+export const WEBSOCKET_URL = process.env.API_ENV === 'production' ?
+  'https://realtime.flex-appeal.nl' : 'https://test.realtime.flex-appeal.nl';
 
 export const send = (eventName, users, payload, token) => {
   const userIds = R.pluck('id', users);
