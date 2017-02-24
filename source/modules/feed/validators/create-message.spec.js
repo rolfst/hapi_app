34a5b40468fd50e103612ea92aba9a0546334f84
rolfst <rolfst@gmail.com>
2017-02-24 16:13:57 +0100

    fix(feed-createMessage): allows either text or an attachment

---
+++ source/modules/feed/validators/create-message.spec.js
@@ -0,0 +1,42 @@
+import { assert } from 'chai';
+import Joi from 'joi';
+import * as testHelper from '../../../shared/test-utils/helpers';
+import scheme from './create-message';
+
+describe('Validator: create-message', () => {
+  it('should succeed when only an attachment is provided', async () => {
+    const hapiFile = testHelper.hapiFile('image.jpg');
+    const payload = {
+      attachments: [hapiFile],
+    };
+    const actual = Joi.validate({ payload }, scheme);
+
+    assert.isNull(actual.error);
+  });
+  it('should succeed when only an attachment is provided', async () => {
+    const hapiFile = testHelper.hapiFile('image.jpg');
+    const payload = {
+      text: 'just a text',
+      attachments: [hapiFile],
+    };
+    const actual = Joi.validate({ payload }, scheme);
+
+    assert.isNull(actual.error);
+  });
+
+  it('should succeed when only a text is provided', async () => {
+    const payload = {
+      text: 'just a text',
+    };
+    const actual = Joi.validate({ payload }, scheme);
+
+    assert.isNull(actual.error);
+  });
+  it('should succeed when no param is provided', async () => {
+    const payload = {
+    };
+    const actual = Joi.validate({ payload }, scheme);
+
+    assert.ifError(actual.error[0]);
+  });
+});
