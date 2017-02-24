5694f73312dfff1dc42ea60cc7ea14b7aa95d8cd
Liam Hubers <liam.hubers@gmail.com>
2017-02-24 16:17:59 +0100

    Improved code styling

--- source/modules/feed/validators/create-message.spec.js
+++ source/modules/feed/validators/create-message.spec.js
@@ -6,36 +6,29 @@ import scheme from './create-message';
 describe('Validator: create-message', () => {
   it('should succeed when only an attachment is provided', async () => {
     const hapiFile = testHelper.hapiFile('image.jpg');
-    const payload = {
-      attachments: [hapiFile],
-    };
+    const payload = { attachments: [hapiFile] };
     const actual = Joi.validate({ payload }, scheme);
 
     assert.isNull(actual.error);
   });
+
   it('should succeed when only an attachment is provided', async () => {
     const hapiFile = testHelper.hapiFile('image.jpg');
-    const payload = {
-      text: 'just a text',
-      attachments: [hapiFile],
-    };
+    const payload = { text: 'just a text', attachments: [hapiFile] };
     const actual = Joi.validate({ payload }, scheme);
 
     assert.isNull(actual.error);
   });
 
   it('should succeed when only a text is provided', async () => {
-    const payload = {
-      text: 'just a text',
-    };
+    const payload = { text: 'just a text' };
     const actual = Joi.validate({ payload }, scheme);
 
     assert.isNull(actual.error);
   });
+
   it('should succeed when no param is provided', async () => {
-    const payload = {
-    };
-    const actual = Joi.validate({ payload }, scheme);
+    const actual = Joi.validate({}, scheme);
 
     assert.ifError(actual.error[0]);
   });
