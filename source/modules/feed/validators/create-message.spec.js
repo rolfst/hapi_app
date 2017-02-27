af97e95e03d8c830677b9834caf4a574e2e5e331
rolfst <rolfst@gmail.com>
2017-02-27 11:55:25 +0100

    chore(feed-createmessage): added extra test for when no body params were provided

--- source/modules/feed/validators/create-message.spec.js
+++ source/modules/feed/validators/create-message.spec.js
@@ -3,7 +3,7 @@ import Joi from 'joi';
 import * as testHelper from '../../../shared/test-utils/helpers';
 import scheme from './create-message';
 
-describe('Validator: create-message', () => {
+describe.only('Validator: create-message', () => {
   it('should succeed when only an attachment is provided', async () => {
     const hapiFile = testHelper.hapiFile('image.jpg');
     const payload = {
@@ -32,8 +32,18 @@ describe('Validator: create-message', () => {
 
     assert.isNull(actual.error);
   });
-  it('should succeed when no param is provided', async () => {
+
+  it('should return error when no param is provided', async () => {
     const payload = {
+    };
+    const actual = Joi.validate({ payload }, scheme);
+
+    assert.ifError(actual.error[0]);
+  });
+
+  it('should return error when unknown param is provided', async () => {
+    const payload = {
+      unknown: 'hi',
     };
     const actual = Joi.validate({ payload }, scheme);
 
