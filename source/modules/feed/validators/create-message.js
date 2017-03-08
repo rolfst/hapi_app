ac796cf3244d8919083ef2704f027c371c36b051
guidsen <guisch95@gmail.com>
2017-03-08 17:00:55 +0100

    Fixed validation issue

--- source/modules/feed/validators/create-message.js
+++ source/modules/feed/validators/create-message.js
@@ -2,7 +2,7 @@ import Joi from 'joi';
 
 export default {
   payload: Joi.object().keys({
-    text: Joi.string().allow(null),
+    text: Joi.string().allow(null).allow(''),
     files: Joi.any(),
     poll_question: Joi.string(),
     poll_options: Joi.array().items(Joi.string(), Joi.number()),
