685393bb5488f8c40ab48a9dfac3505648c85170
rolfst <rolfst@gmail.com>
2017-03-10 15:25:04 +0100

    chore(statistics): created getCreatedMessages

---
+++ source/modules/statistics/services/event/index.js
@@ -0,0 +1,18 @@
+import moment from 'moment';
+import * as eventRepo from '../../repositories/event';
+import * as Logger from '../../../shared/services/logger';
+
+const logger = Logger.createLogger('STATISTICS/service/events');
+
+async function getMessagesStats(event, networkId, startDate, endDate) {
+  return eventRepo.findAllBy({ event, networkId, startDate, endDate });
+}
+
+export async function getCreatedMessages(payload, message) {
+  logger.info('Retrieving Created Messages', { payload, message });
+
+  const startDate = payload.startDate || moment().subtract(1, 'month').toDate();
+  const endDate = payload.endDate || moment().toDate();
+
+  const stats = await getMessagesStats('Created Message', payload.networkId, startDate, endDate);
+}
