ee72900164cac2e545cf10f28cb7da7dd0ab223f
rolfst <rolfst@gmail.com>
2017-03-13 16:22:53 +0100

    refactor(statistics): renamed handler name

--- source/modules/statistics/create-routes.js
+++ source/modules/statistics/create-routes.js
@@ -3,7 +3,7 @@ import createRoutes from '../../shared/utils/create-routes';
 const routes = [{
   method: 'GET',
   url: '/v2/networks/{networkId}/statistics/{viewName}',
-  handler: require('./handlers/created-messages'),
+  handler: require('./handlers/events'),
   validator: require('./validators/events'),
 }];
 
