b19719ce7263fa82bde46b8b9771401d8bdf96fc
rolfst <rolfst@gmail.com>
2017-03-13 16:46:57 +0100

    refactor(statistics): cleaned up handler conditional

--- source/modules/statistics/handlers/events.js
+++ source/modules/statistics/handlers/events.js
@@ -3,8 +3,6 @@ import createError from '../../../shared/utils/create-error';
 import * as responseUtil from '../../../shared/utils/response';
 import * as eventService from '../services/event';
 
-const badViewHandler = () => { throw createError('500'); };
-
 export default async (req, reply) => {
   try {
     const message = { ...req.pre, ...req.auth };
@@ -12,8 +10,10 @@ export default async (req, reply) => {
 
     const handler = R.cond([
       [R.path(['params', 'viewName']), () => eventService.getCreatedMessages],
-      [R.T, () => badViewHandler], // this should never have passed the validation
+      [R.T, () => null], // this should never have passed the validation
     ])(req);
+
+    if (!handler) throw createError('500');
 
     const stats = await handler(payload, message);
 
