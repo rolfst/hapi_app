9431d6bfd4d2985c543191ffefdf83cf0f20fd3c
rolfst <rolfst@gmail.com>
2017-03-13 16:41:28 +0100

    refactor(statistics): cleaned up handler redirection

--- source/modules/statistics/handlers/events.js
+++ source/modules/statistics/handlers/events.js
@@ -1,20 +1,21 @@
+import R from 'ramda';
 import createError from '../../../shared/utils/create-error';
 import * as responseUtil from '../../../shared/utils/response';
 import * as eventService from '../services/event';
+
+const badViewHandler = () => { throw createError('500'); };
 
 export default async (req, reply) => {
   try {
     const message = { ...req.pre, ...req.auth };
     const payload = { ...req.params, ...req.query };
 
-    let stats;
+    const handler = R.cond([
+      [R.path(['params', 'viewName']), () => eventService.getCreatedMessages],
+      [R.T, () => badViewHandler], // this should never have passed the validation
+    ])(req);
 
-    if (req.params.viewName === 'created_message') {
-      stats = await eventService.getCreatedMessages(payload, message);
-    } else {
-      throw createError('500'); // this should never have passed the validation
-    }
-
+    const stats = await handler(payload, message);
 
     return reply({ data: responseUtil.toSnakeCase(stats.payload) });
   } catch (err) {
