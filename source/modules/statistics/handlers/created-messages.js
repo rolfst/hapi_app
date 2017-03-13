b0e68bb42bafbca3d9fec67686c7bb814ce8fb7c
rolfst <rolfst@gmail.com>
2017-03-13 16:20:37 +0100

    chore(statistics): added redirection in handler

--- source/modules/statistics/handlers/created-messages.js
+++ source/modules/statistics/handlers/created-messages.js
@@ -1,12 +1,20 @@
-import * as eventService from '../services/event';
+import createError from '../../../shared/utils/create-error';
 import * as responseUtil from '../../../shared/utils/response';
+import * as eventService from '../services/event';
 
 export default async (req, reply) => {
   try {
     const message = { ...req.pre, ...req.auth };
     const payload = { ...req.params, ...req.query };
 
-    const stats = await eventService.getCreatedMessages(payload, message);
+    let stats;
+
+    if (req.params.viewName === 'created_message') {
+      stats = await eventService.getCreatedMessages(payload, message);
+    } else {
+      throw createError('500'); // this should never have passed the validation
+    }
+
 
     return reply({ data: responseUtil.toSnakeCase(stats.payload) });
   } catch (err) {
