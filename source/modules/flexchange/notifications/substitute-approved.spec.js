bee0a0de5de0d7a8c2e285dbc2c09270a36fed8b
guidsen <guisch95@gmail.com>
2017-03-15 13:10:17 +0100

    Fixed timezone bug for summer time transition

--- source/modules/flexchange/notifications/substitute-approved.spec.js
+++ source/modules/flexchange/notifications/substitute-approved.spec.js
@@ -11,14 +11,16 @@ describe('Substitude flexchange notification', () => {
     User: { id: 2, fullName: 'John Doe' },
   });
 
-  const localTime = {
-    startTime: moment(moment().hour(10).minute(30)).tz('Europe/Amsterdam').format('HH:mm'),
-    endTime: moment(moment().hour(13).minute(0)).tz('Europe/Amsterdam').format('HH:mm'),
-  };
+  const getLocalTime = (baseMoment) => ({
+    startTime: moment(baseMoment.hour(10).minute(30)).tz('Europe/Amsterdam').format('HH:mm'),
+    endTime: moment(baseMoment.hour(13).minute(0)).tz('Europe/Amsterdam').format('HH:mm'),
+  });
+
 
   it('should return a correct notification object for today', () => {
     const futureMoment = moment();
     const exchange = createExchange(futureMoment);
+    const localTime = getLocalTime(futureMoment);
 
     const actual = createNotification(exchange);
     const expected = {
@@ -33,6 +35,7 @@ describe('Substitude flexchange notification', () => {
   it('should return a correct notification object for tomorrow', () => {
     const futureMoment = moment().add(1, 'days');
     const exchange = createExchange(futureMoment);
+    const localTime = getLocalTime(futureMoment);
 
     const actual = createNotification(exchange);
     const expected = {
@@ -47,6 +50,7 @@ describe('Substitude flexchange notification', () => {
   it('should return a correct notification object for this week', () => {
     const futureMoment = moment().add(2, 'days');
     const exchange = createExchange(futureMoment);
+    const localTime = getLocalTime(futureMoment);
 
     const actual = createNotification(exchange);
     const expected = {
@@ -61,6 +65,7 @@ describe('Substitude flexchange notification', () => {
   it('should return a correct notification object for later than this week', () => {
     const futureMoment = moment().add(2, 'weeks');
     const exchange = createExchange(futureMoment);
+    const localTime = getLocalTime(futureMoment);
 
     const actual = createNotification(exchange);
     const expected = {
