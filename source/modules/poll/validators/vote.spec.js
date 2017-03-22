116e67940da11364c7b676f642a45db0bad350be
Guido Schmitz <guisch95@gmail.com>
2017-03-22 10:25:00 +0100

    Removed whitelines

--- source/modules/poll/validators/vote.spec.js
+++ source/modules/poll/validators/vote.spec.js
@@ -5,7 +5,6 @@ import scheme from './vote';
 describe('Validator: vote', () => {
   it('should succeed when receiving option_ids', () => {
     const payload = { option_ids: ['1', '2'] };
-
     const actual = Joi.validate({ payload }, scheme);
 
     assert.isNull(actual.error);
@@ -13,7 +12,6 @@ describe('Validator: vote', () => {
 
   it('should fail when option_ids doesn\'t exist', () => {
     const payload = { options: [] };
-
     const actual = Joi.validate({ payload }, scheme);
 
     assert.ifError(actual.error[0]);
@@ -21,7 +19,6 @@ describe('Validator: vote', () => {
 
   it('should fail when option_ids is empty', () => {
     const payload = { option_ids: [] };
-
     const actual = Joi.validate({ payload }, scheme);
 
     assert.ifError(actual.error[0]);
