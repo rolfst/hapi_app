12765ffb49d92c4f869bf36e3f34ea24565850b8
guidsen <guisch95@gmail.com>
2017-03-27 16:20:05 +0200

    Fixed file upload validation

--- source/modules/attachment/validators/upload-file.js
+++ source/modules/attachment/validators/upload-file.js
@@ -2,6 +2,6 @@ const Joi = require('joi');
 
 module.exports = {
   payload: Joi.object().keys({
-    file: Joi.any(),
+    fileStream: Joi.any().label('file'),
   }).rename('file', 'fileStream'),
 };
