55404465c94559b916f5f8a07d05f46d73ea22e2
guidsen <guisch95@gmail.com>
2017-03-27 15:02:26 +0200

    Check for value before reading

--- source/modules/feed/handlers/get-message.js
+++ source/modules/feed/handlers/get-message.js
@@ -5,7 +5,7 @@ const messageService = require('../services/message');
 module.exports = async (req, reply) => {
   try {
     const { payload, message } = createServicePayload(req);
-    payload.include = req.query.include.split(',');
+    payload.include = req.query.include ? req.query.include.split(',') : [];
     const likes = await messageService.getAsObject(payload, message);
 
     return reply({ data: responseUtil.toSnakeCase(likes) });
