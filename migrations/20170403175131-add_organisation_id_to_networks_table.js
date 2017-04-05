6bc78f863d4f9ded541b9a056b885e215a103504
guidsen <guisch95@gmail.com>
2017-04-05 11:06:32 +0200

    Added migrations

---
+++ migrations/20170403175131-add_organisation_id_to_networks_table.js
@@ -0,0 +1,25 @@
+'use strict';
+
+module.exports = {
+  up: function (queryInterface, Sequelize) {
+    return queryInterface.addColumn('networks', 'organisation_id', {
+      type: Sequelize.INTEGER.UNSIGNED,
+      allowNull: true,
+      references: {
+        model: 'organisations',
+        key: 'id',
+      },
+      onDelete: 'set null',
+    });
+  },
+
+  down: function (queryInterface, Sequelize) {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.dropTable('users');
+    */
+  }
+};
---
+++ migrations/20170405080232-add_organisation_functions_table.js
@@ -0,0 +1,33 @@
+'use strict';
+
+module.exports = {
+  up: function (queryInterface, Sequelize) {
+    return queryInterface.createTable('organisation_functions', {
+      id: {
+        type: Sequelize.INTEGER.UNSIGNED,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      name: {
+        type: Sequelize.STRING,
+        defaultValue: null,
+        allowNull: true,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+        allowNull: false,
+      },
+    });
+  },
+
+  down: function (queryInterface, Sequelize) {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.dropTable('users');
+    */
+  }
+};
