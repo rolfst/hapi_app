2c040085b923f3f4dc7c886f1c3318b23f077dde
guidsen <guisch95@gmail.com>
2017-04-06 08:36:48 +0200

    Added e2e test for endpoint

---
+++ source/modules/core/handlers/networks-for-organisation.e2e.js
@@ -0,0 +1,44 @@
+const { assert } = require('chai');
+const testHelpers = require('../../../shared/test-utils/helpers');
+const { getRequest } = require('../../../shared/test-utils/request');
+
+describe('Handler: Networks for organisation', () => {
+  let organisation;
+  let admin;
+  let otherUser;
+
+  before(async () => {
+    organisation = await testHelpers.createOrganisation();
+    admin = await testHelpers.createUser();
+    otherUser = await testHelpers.createUser();
+
+    await testHelpers.addUserToOrganisation(admin.id, organisation.id, 'ADMIN');
+
+    return Promise.all([
+      testHelpers.createNetwork({
+        userId: admin.id,
+        organisationId: organisation.id,
+      }),
+      testHelpers.createNetwork({
+        userId: admin.id,
+        organisationId: organisation.id,
+      }),
+    ]);
+  });
+
+  after(() => testHelpers.cleanAll());
+
+  it('should return all networks for organisation', async () => {
+    const endpoint = `/v2/organisations/${organisation.id}/networks`;
+    const { result } = await getRequest(endpoint, admin.token);
+
+    assert.lengthOf(result.data, 2);
+  });
+
+  it('should fail when user doesnt belong to the organisation', async () => {
+    const endpoint = `/v2/organisations/${organisation.id}/networks`;
+    const { statusCode } = await getRequest(endpoint, otherUser.token);
+
+    assert.equal(statusCode, 403);
+  });
+});
