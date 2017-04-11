5ff73c418875a535374017f8a0f1bac72a6b7bab
guidsen <guisch95@gmail.com>
2017-04-11 14:23:11 +0200

    Added headings config to notifications

--- source/modules/feed/notifications/message-created.js
+++ source/modules/feed/notifications/message-created.js
@@ -1,10 +1,7 @@
 module.exports = (actor, parent, object) => {
-  const text = parent.name ?
-    `${actor.fullName} in ${parent.name}: ${object.source.text}` :
-    `${actor.fullName}: ${object.source.text}`;
-
   return {
-    text,
+    headings: parent.name ? `${actor.fullName} in ${parent.name}` : `${actor.fullName}`,
+    text: object.source.text,
     data: { id: object.source.id, type: 'message', track_name: 'message_created' },
   };
 };
