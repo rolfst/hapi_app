59a3fab6f589de3566008fc39b07897355bea91e
Dave Derichs <dave@flex-appeal.nl>
2017-04-12 17:43:21 +0200

    Cleanup

--- source/modules/workflow/repositories/dao/index.js
+++ source/modules/workflow/repositories/dao/index.js
@@ -1,8 +1,34 @@
+const Organisation = require('../../../core/repositories/dao/organisation');
+const User = require('../../../core/repositories/dao/user');
 const WorkFlow = require('./workflow');
 const Trigger = require('./trigger');
 const Condition = require('./condition');
 const Action = require('./action');
 const ActionDone = require('./action-done');
+
+WorkFlow.belongsTo(Organisation, {
+  foreignKey: 'organisation_id',
+});
+
+Trigger.belongsTo(WorkFlow, {
+  foreignKey: 'workflow_id',
+});
+
+Condition.belongsTo(WorkFlow, {
+  foreignKey: 'workflow_id',
+});
+
+Action.belongsTo(WorkFlow, {
+  foreignKey: 'workflow_id',
+});
+
+ActionDone.belongsTo(WorkFlow, {
+  foreignKey: 'workflow_id',
+});
+
+ActionDone.belongsTo(WorkFlow, {
+  foreignKey: 'user_id',
+});
 
 exports.WorkFlow = WorkFlow;
 exports.Trigger = Trigger;
--- source/modules/workflow/repositories/workflow.js
+++ source/modules/workflow/repositories/workflow.js
@@ -6,35 +6,25 @@ const Condition2Model = require('../models/condition');
 const Action2Model = require('../models/action');
 const ActionDone2Model = require('../models/actiondone');
 
-const findAll = (workflowIdsOrWhereConstraints) => {
-  const whereConstraints =
-    typeof workflowIdsOrWhereConstraints === 'object'
-    && !Array.isArray(workflowIdsOrWhereConstraints)
-      ? workflowIdsOrWhereConstraints
-      : { id: workflowIdsOrWhereConstraints };
+const buildWhereConstraint = (idOrWhereConstraint) =>
+  typeof idOrWhereConstraint === 'object'
+  && !Array.isArray(idOrWhereConstraint)
+    ? idOrWhereConstraint
+    : { id: idOrWhereConstraint };
 
+const findAll = (workflowIdsOrWhereConstraints) => {
   return WorkFlow
-    .findAll({ where: whereConstraints })
+    .findAll({ where: buildWhereConstraint(workflowIdsOrWhereConstraints) })
     .then(R.map(WorkFlow2Model));
 };
 
 const findOne = (workflowIdOrWhereConstraints) => {
-  const whereConstraints = typeof workflowIdOrWhereConstraints === 'object'
-    ? workflowIdOrWhereConstraints
-    : { id: [workflowIdOrWhereConstraints] };
-
-  return R.head(findAll(whereConstraints));
+  return R.head(findAll(buildWhereConstraint(workflowIdOrWhereConstraints)));
 };
 
 const findAllWithData = async (workflowIdsOrWhereConstraints) => {
-  const whereConstraints =
-    typeof workflowIdsOrWhereConstraints === 'object'
-    && !Array.isArray(workflowIdsOrWhereConstraints)
-      ? workflowIdsOrWhereConstraints
-      : { id: workflowIdsOrWhereConstraints };
-
   const workFlows = await WorkFlow
-    .findAll({ where: whereConstraints })
+    .findAll({ where: buildWhereConstraint(workflowIdsOrWhereConstraints) })
     .then(R.map(WorkFlow2Model));
 
   const dataFindOptions = { where: { workflowId: R.pluck('id', workFlows) } };
@@ -49,24 +39,19 @@ const findAllWithData = async (workflowIdsOrWhereConstraints) => {
   const findConditions = (workflowId) => R.filter(R.propEq('workflowId', workflowId), conditions);
   const findActions = (workflowId) => R.filter(R.propEq('workflowId', workflowId), actions);
 
-  const addData = (workflow) =>
-    R.pipe(
-      R.assoc('triggers', findTriggers(workflow.id)),
-      R.assoc('conditions', findConditions(workflow.id)),
-      R.assoc('actions', findActions(workflow.id)),
-    );
+  const addData = (workflow) => R.merge(workflow, {
+    triggers: findTriggers(workflow.id),
+    conditions: findConditions(workflow.id),
+    actions: findActions(workflow.id),
+  });
 
   return R.map(addData, workFlows);
 };
 
 const findOneWithData = (workflowIdOrWhereConstraints) => {
-  const whereConstraints =
-    typeof workflowIdOrWhereConstraints === 'object'
-      ? workflowIdOrWhereConstraints
-      : { id: [workflowIdOrWhereConstraints] };
-
-  return R.head(findAllWithData(whereConstraints));
+  return R.head(findAllWithData(buildWhereConstraint(workflowIdOrWhereConstraints)));
 };
+
 exports.findAll = findAll;
 exports.findOne = findOne;
 exports.findAllWithData = findAllWithData;
