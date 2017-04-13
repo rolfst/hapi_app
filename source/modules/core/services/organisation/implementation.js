462e400844f5d2e1e49486a0ce93b4e2f9dde54a
rolfst <rolfst@gmail.com>
2017-04-13 09:46:22 +0200

    chore(organisation): created organisation asserts

---
+++ source/modules/core/services/organisation/implementation.js
@@ -0,0 +1,25 @@
+const createError = require('../../../../shared/utils/create-error');
+const organisationRepo = require('../../repositories/organisation');
+
+async function assertThatUserIsMemberOfOrganisation(userId, organisationId) {
+    if (!await organisationRepo.hasUser(userId, organisationId)) {
+    throw createError('403');
+  }
+    const userMeta = await organisationRepo.getPivot(userId, organisationId);
+    if (!userMeta) throw createError('403', 'User is not a member of the organisation');
+}
+
+async function assertThatUserIsAdminInOrganisation(userId, organisationId) {
+    const userMeta = await organisationRepo.getPivot(userId, organisationId);
+
+    if (!userMeta || userMeta.roleType !== 'ADMIN') throw createError('10020', 'User is not an admin in the organisation');
+}
+
+async function assertThatOrganisationExists(organisationId) {
+  const organisation = await organisationRepository.findById(payload.organisationId);
+  if (!organisation) throw createError('404', 'Organisation not found.'); 
+}
+
+exports.assertThatOrganisationExists = assertThatOrganisationExists;
+exports.assertThatUserIsMemberOfOrganisation = assertThatUserIsMemberOfOrganisation;
+exports.assertThatUserIsAdminInOrganisation = assertThatUserIsAdminInOrganisation;
