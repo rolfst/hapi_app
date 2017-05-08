368a865b4ff03db4b52e1d5c0106104b740bc5c4
Dave Derichs <dave@flex-appeal.nl>
2017-05-08 11:59:27 +0200

    cyclic dependency fixed

--- source/modules/workflow/services/processor.js
+++ source/modules/workflow/services/processor.js
@@ -1,4 +1,5 @@
 const R = require('ramda');
+const sequelize = require('../../../shared/configs/sequelize');
 const logger = require('../../../shared/services/logger')('WORKFLOW/service/processor');
 
 const baseQuery = `
@@ -8,7 +9,7 @@ FROM
   organisation_user ou
   %joins
 WHERE
-  ou.organisation_id = :organisation_id
+  ou.organisation_id = %organisation_id
   %where
 GROUP BY
   ou.user_id
@@ -17,7 +18,7 @@ GROUP BY
 const structure = {
   user: {
     identifier: 'u',
-    joinSQL: 'LEFT JOIN user u ON u.id = ou.user_id',
+    joinSQL: 'LEFT JOIN users u ON u.id = ou.user_id',
     fields: [
       'id',
       'username',
@@ -45,7 +46,8 @@ const structure = {
     depends: [
       'network',
     ],
-    joinSQL: 'LEFT JOIN network_user nu ON (ou.user_id = nu.user_id AND n.organisation_id = ou.organisation_id)',
+    // The join for network is also here because otherwise it would create a cyclic dependency
+    joinSQL: 'LEFT JOIN networks n ON n.organisation_id = ou.organisation_id\n  LEFT JOIN network_user nu ON (nu.network_id = n.id AND ou.user_id = nu.user_id)',
     fields: [
       'id',
       'role_type',
@@ -59,7 +61,7 @@ const structure = {
     depends: [
       'network_user',
     ],
-    joinSQL: 'LEFT JOIN network n ON (n.organisation_id = ou.organisation_id AND ou.user_id = nu.user_id)',
+    joinSQL: '',
     fields: [
       'id',
       'name',
@@ -86,17 +88,22 @@ const buildQuery = (organisationId, conditions) => {
   logger.info('buildQuery', { organisationId, conditions });
 
   const joins = [];
-  const addJoin = (name) => {
+  const addJoin = (name, skipDependency) => {
     if (joins.includes(name)) return;
 
     if (!structure.hasOwnProperty(name)) throw new Error('invalid join');
 
     if (structure[name].hasOwnProperty('depends')) {
-      structure[name].depends.forEach(addJoin);
+      R.forEach((depName) => {
+        if (depName !== skipDependency) addJoin(depName, name);
+      }, structure[name].depends);
     }
 
     joins.push(name);
   };
+
+  addJoin('user');
+  addJoin('network');
 
   const whereConditions = [];
   const params = {
@@ -111,14 +118,12 @@ const buildQuery = (organisationId, conditions) => {
     }
 
     return null;
-  }), joins);
+  }, joins));
 
-  return {
-    query: baseQuery
-      .replace('%joins', buildJoins.join(' '))
-      .replace('%where', whereConditions.join(' AND ')),
-    params,
-  }
+  return baseQuery
+    .replace('%joins', buildJoins.join('\n  '))
+    .replace('%where', whereConditions.join(' AND '))
+    .replace('%organisation_id', sequelize.escape(organisationId));
 };
 
 exports.buildQuery = buildQuery;
