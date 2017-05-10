81892a71069a50aaacbf4ec33568e968efb0d982
Liam Hubers <liam.hubers@gmail.com>
2017-05-10 14:23:21 +0200

    Added tests for notifier

--- source/shared/services/notifier.spec.js
+++ source/shared/services/notifier.spec.js
@@ -1,23 +1,57 @@
 const Mixpanel = require('mixpanel');
 const { assert } = require('chai');
 const sinon = require('sinon');
-const Parse = require('parse/node');
 const notifier = require('./notifier');
 
 describe('Notifier', () => {
+  const mixpanelClient = Mixpanel.init('foo_token');
   let sandbox;
 
+  before(() => {
+    sandbox = sinon.sandbox.create();
+
+    sandbox.stub(Mixpanel, 'init').returns(mixpanelClient);
+    sandbox.stub(mixpanelClient, 'track');
+    sandbox.stub(notifier, 'sendNotification')
+      .returns(new Promise((resolve) => resolve({ ok: true })));
+  });
   afterEach(() => sandbox.restore());
-  beforeEach(() => { sandbox = sinon.sandbox.create(); });
 
   describe('send', () => {
-    it.skip('should track push notifications', () => {
-      const mixpanelClient = Mixpanel.init('foo_token');
-      sandbox.stub(Mixpanel, 'init').returns(mixpanelClient);
-      const methodSpy = sandbox.stub(mixpanelClient, 'track');
-      sandbox.stub(Parse.Push, 'send').returns(Promise.resolve(null));
+    it('should create chunks', () => {
+      const users = [
+        { email: 'foo+1@flex-appeal.nl' },
+        { email: 'foo+2@flex-appeal.nl' },
+        { email: 'foo+3@flex-appeal.nl' },
+      ];
 
-      const notificationStub = {
+      const expected = [
+        ['foo+1@flex-appeal.nl', 'foo+2@flex-appeal.nl'],
+        ['foo+3@flex-appeal.nl'],
+      ];
+      const actual = notifier.createEmailChunks(users, 2);
+
+      assert.deepEqual(actual, expected);
+    });
+
+    it('should create correct data', () => {
+      const notification = { data: { foo: 'test' } };
+      const emails = ['foo+1@flex-appeal.nl', 'foo+2@flex-appeal.nl'];
+      const payload = { organisationId: '1', networkId: '2' };
+
+      const expected = [
+        { field: 'email', value: 'foo+1@flex-appeal.nl' },
+        { operator: 'OR' },
+        { field: 'email', value: 'foo+2@flex-appeal.nl' },
+      ];
+      const actual = notifier.createData(notification, payload, emails);
+
+      assert.deepEqual(actual.data, { foo: 'test', organisation_id: '1', network_id: '2' });
+      assert.deepEqual(actual.filters, expected);
+    });
+
+    it.skip('should track push notifications', () => {
+      const notifications = {
         text: 'Foo notification text',
         data: { id: '1', type: 'foo', track_name: 'foo_track_id' },
       };
@@ -27,9 +61,9 @@ describe('Notifier', () => {
         { id: '2', username: 'test2@flex-appeal.nl', email: 'test2@flex-appeal.nl' },
       ];
 
-      notifier.send(users, notificationStub);
+      notifier.send(users, notifications).catch(console.log);
 
-      assert.equal(methodSpy.callCount, 2);
+      assert.equal(mixpanelClient.track.callCount, 2);
     });
   });
 });
