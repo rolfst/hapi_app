c77d17374530aaba8c6300bebb7a87cad6485163
Dave Derichs <dave@flex-appeal.nl>
2017-05-11 15:46:02 +0200

    the worker is almost completely implemented

--- source/modules/workflow/worker/index.js
+++ source/modules/workflow/worker/index.js
@@ -1,5 +1,44 @@
-const run = () => {
+/* eslint no-use-before-define: ["error", { "functions": false }] */
+const impl = require('./implementation');
+const { WORKER_INTERVAL } = require('../definitions');
 
-};
+let timer;
+let running = false;
+let shuttingDown = false;
 
-module.exports = run;
+function scheduleNextRun() {
+  if (shuttingDown) return;
+
+  timer = setTimeout(run, WORKER_INTERVAL);
+}
+
+function run() {
+  if (running || shuttingDown) return;
+
+  if (!timer) {
+    return scheduleNextRun();
+  }
+
+  running = true;
+
+  impl
+    .fetchAndProcessWorkflows()
+    .finally(() => {
+      running = false;
+
+      // we're testing, so stop after one run
+      if (process.env.API_ENV === 'testing') return;
+
+      scheduleNextRun();
+    });
+}
+
+function shutdown() {
+  shuttingDown = true;
+}
+
+exports = run;
+exports.start = run;
+exports.shutdown = shutdown;
+
+module.exports = exports;
