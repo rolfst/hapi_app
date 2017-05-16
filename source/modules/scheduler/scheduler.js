bd45a35d9d4cdaebc3296335f78d4a8d32362bec
Liam Hubers <liam.hubers@gmail.com>
2017-05-16 14:13:06 +0200

    Temporarily disable weekly update scheduler (#557)
    * Temporarily disable weekly update scheduler
    
    * Update scheduler.js
    
    Fixed lint errors

--- source/modules/scheduler/scheduler.js
+++ source/modules/scheduler/scheduler.js
@@ -1,11 +1,11 @@
 const CronJob = require('cron').CronJob;
-const R = require('ramda');
+// const R = require('ramda');
 const createSentryClient = require('../../shared/services/sentry');
-const networkRepo = require('../core/repositories/network');
+// const networkRepo = require('../core/repositories/network');
 const syncService = require('../integrations/services/sync');
-const weeklyUpdate = require('./weekly-update');
+// const weeklyUpdate = require('./weekly-update');
 
-const logger = require('../../shared/services/logger')('MODULE/scheduler');
+// const logger = require('../../shared/services/logger')('MODULE/scheduler');
 
 const syncJob = new CronJob({
   cronTime: '00 30 * * * *', // Every 30 minutes
@@ -26,18 +26,18 @@ const syncJob = new CronJob({
   timeZone: 'Europe/Amsterdam',
 });
 
-const weeklyUpdateJob = new CronJob({
-  cronTime: '00 13 * * 0', // Every sunday at 13:00
-  onTick() {
-    networkRepo.findAll()
-      .then(R.pipe(R.pluck('id'), R.map(weeklyUpdate.send)))
-      .catch((err) => {
-        logger.error('Weekly update failed', err);
-        // Don't rethrow so the process will not die
-      });
-  },
-  timeZone: 'Europe/Amsterdam',
-});
+// const weeklyUpdateJob = new CronJob({
+//   cronTime: '00 13 * * 0', // Every sunday at 13:00
+//   onTick() {
+//     networkRepo.findAll()
+//       .then(R.pipe(R.pluck('id'), R.map(weeklyUpdate.send)))
+//       .catch((err) => {
+//         logger.error('Weekly update failed', err);
+//         // Don't rethrow so the process will not die
+//       });
+//   },
+//   timeZone: 'Europe/Amsterdam',
+// });
 
 syncJob.start();
-weeklyUpdateJob.start();
+// weeklyUpdateJob.start();
