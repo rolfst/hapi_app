0834c97d43da4413cf7aa506604b2be168dce8d4
Dave Derichs <dave@flex-appeal.nl>
2017-05-22 14:05:19 +0200

    no use for conditions here

--- source/modules/feed/handlers/create-organisation-message.js
+++ source/modules/feed/handlers/create-organisation-message.js
@@ -1,6 +1,7 @@
 const createServicePayload = require('../../../shared/utils/create-service-payload');
 const messageService = require('../services/message');
 const responseUtil = require('../../../shared/utils/response');
+const createError = require('../../../shared/utils/create-error');
 const organisationService = require('../../core/services/organisation');
 const { EMessageTypes } = require('../definitions');
 
@@ -8,8 +9,14 @@ module.exports = async (req, reply) => {
   try {
     const { payload, message } = createServicePayload(req);
 
-    await organisationService
-      .userHasRoleInOrganisation(payload.organisationId, message.credentials.id);
+    if (
+      !(await organisationService.userHasRoleInOrganisation(
+        payload.organisationId,
+        message.credentials.id,
+        organisationService.ERoleTypes.ADMIN))
+    ) {
+      throw createError('403');
+    }
 
     payload.parentType = 'organisation';
     payload.parentId = payload.organisationId;
