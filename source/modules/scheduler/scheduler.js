2d5db14d4acedb8cd555e397ed781aa7b430865c
Rolf Strijdhorst <rolfst@gmail.com>
2017-05-24 09:51:00 +0200

    doc(scheduler): removed commented out code

--- source/modules/scheduler/scheduler.js
+++ source/modules/scheduler/scheduler.js
@@ -1,9 +1,6 @@
 const CronJob = require('cron').CronJob;
-// const R = require('ramda');
 const createSentryClient = require('../../shared/services/sentry');
-// const networkRepo = require('../core/repositories/network');
 const syncService = require('../integrations/services/sync');
-// const weeklyUpdate = require('./weekly-update');
 const removeShifts = require('./remove-shifts');
 
 const logger = require('../../shared/services/logger')('MODULE/scheduler');
@@ -27,19 +24,6 @@ const syncJob = new CronJob({
   timeZone: 'Europe/Amsterdam',
 });
 
-// const weeklyUpdateJob = new CronJob({
-//   cronTime: '00 13 * * 0', // Every sunday at 13:00
-//   onTick() {
-//     networkRepo.findAll()
-//       .then(R.pipe(R.pluck('id'), R.map(weeklyUpdate.send)))
-//       .catch((err) => {
-//         logger.error('Weekly update failed', err);
-//         // Don't rethrow so the process will not die
-//       });
-//   },
-//   timeZone: 'Europe/Amsterdam',
-// });
-
 const removeOutdatedShiftsJob = new CronJob({
   cronTime: '05 * * * *', // Every day at 0:05
   onTick() {
@@ -56,5 +40,4 @@ const removeOutdatedShiftsJob = new CronJob({
 });
 
 syncJob.start();
-// weeklyUpdateJob.start();
 removeOutdatedShiftsJob.start();
