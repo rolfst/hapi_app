d3893d095e859cd592aec2cf7f37f1da0d40a967
guidsen <guisch95@gmail.com>
2017-05-24 11:25:58 +0200

    Changed production env variables to avoid mistakes

--- source/shared/configs/database.js
+++ source/shared/configs/database.js
@@ -9,6 +9,7 @@ exports.testing = {
   dialect: 'mysql',
   migrationStorageTableName: 'sequelize_migrations',
 };
+
 exports.development = {
   host: process.env.DB_HOST,
   database: process.env.DB_NAME,
@@ -18,6 +19,7 @@ exports.development = {
   dialect: 'mysql',
   migrationStorageTableName: 'sequelize_migrations',
 };
+
 exports.acceptance = {
   host: process.env.DB_HOST,
   database: process.env.DB_NAME,
@@ -27,12 +29,13 @@ exports.acceptance = {
   dialect: 'mysql',
   migrationStorageTableName: 'sequelize_migrations',
 };
+
 exports.production = {
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+  host: process.env.PRODUCTION_DB_HOST,
+  database: process.env.PRODUCTION_DB_NAME,
+  username: process.env.PRODUCTION_DB_USERNAME,
+  password: process.env.PRODUCTION_DB_PASSWORD,
+  port: process.env.PRODUCTION_DB_PORT,
   dialect: 'mysql',
   migrationStorageTableName: 'sequelize_migrations',
 };
