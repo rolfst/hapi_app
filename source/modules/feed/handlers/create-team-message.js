769408de56858703a98f75551aa816ddd756781e
guidsen <guisch95@gmail.com>
2017-05-26 21:24:49 +0200

    Fixed bug

--- source/modules/feed/handlers/create-team-message.js
+++ source/modules/feed/handlers/create-team-message.js
@@ -2,13 +2,15 @@ const R = require('ramda');
 const createServicePayload = require('../../../shared/utils/create-service-payload');
 const messageService = require('../services/message');
 const teamService = require('../../core/services/team');
+const networkService = require('../../core/services/network');
 const responseUtil = require('../../../shared/utils/response');
 
 module.exports = async (req, reply) => {
   try {
     const { payload, message } = createServicePayload(req);
 
-    const network = await teamService.findById(req.params.teamId);
+    const team = await teamService.get({ teamId: req.params.teamId }, message);
+    const network = await networkService.get({ networkId: team.networkId }, message);
 
     payload.parentType = 'team';
     payload.parentId = req.params.teamId;
