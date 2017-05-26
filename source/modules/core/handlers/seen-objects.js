63e386f1e22efab210b4bde9264fbe8851b9d59a
guidsen <guisch95@gmail.com>
2017-05-26 15:18:54 +0200

    Moved error handling from handler to service

--- source/modules/core/handlers/seen-objects.js
+++ source/modules/core/handlers/seen-objects.js
@@ -1,8 +1,6 @@
 const responseUtil = require('../../../shared/utils/response');
 const createServicePayload = require('../../../shared/utils/create-service-payload');
 const objectService = require('../services/object');
-const Sequelize = require('sequelize');
-const createError = require('../../../shared/utils/create-error');
 
 module.exports = async (req, reply) => {
   try {
@@ -11,10 +9,6 @@ module.exports = async (req, reply) => {
 
     return reply({ data: responseUtil.toSnakeCase(data) });
   } catch (err) {
-    if (err instanceof Sequelize.UniqueConstraintError) {
-      return reply(createError(50001, 'You cannot read the same object twice ;).'));
-    }
-
     return reply(err);
   }
 };
