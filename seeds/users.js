428d646cdbacbb1be5a2b5a870c511e0439f6ab2
guidsen <guisch95@gmail.com>
2017-05-29 14:37:10 +0200

    Added extra seed user + added profileImgs

--- seeds/users.js
+++ seeds/users.js
@@ -3,24 +3,35 @@ module.exports = [{
   lastName: 'Organisatie Beheerder',
   email: 'organisatiebeheerder-1@flex-appeal.nl',
   roleType: 'ADMIN',
+  profileImg: 'organisatiebeheerder-1.jpg',
 }, {
   firstName: 'Johan',
   lastName: 'Beheerder',
   email: 'beheerder-1@flex-appeal.nl',
   roleType: 'ADMIN',
+  profileImg: 'beheerder-1.jpg',
 }, {
   firstName: 'Amber',
   lastName: 'Beheerder',
   email: 'beheerder-2@flex-appeal.nl',
   roleType: 'ADMIN',
+  profileImg: 'beheerder-2.jpg',
 }, {
   firstName: 'Piet',
   lastName: 'Medewerker',
   email: 'medewerker-1@flex-appeal.nl',
   roleType: 'EMPLOYEE',
+  profileImg: 'medewerker-1.jpg',
 }, {
   firstName: 'Jantje',
   lastName: 'Medewerker',
   email: 'medewerker-2@flex-appeal.nl',
   roleType: 'EMPLOYEE',
+  profileImg: 'medewerker-2.jpg',
+}, {
+  firstName: 'Thomas',
+  lastName: 'Medewerker',
+  email: 'medewerker-3@flex-appeal.nl',
+  roleType: 'EMPLOYEE',
+  profileImg: 'medewerker-3.jpg',
 }];
