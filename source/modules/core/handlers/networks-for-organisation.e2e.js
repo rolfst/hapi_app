f3e597ff9941d91bd90ff793ad0a72b3b5efa950
Guido Schmitz <guisch95@gmail.com>
2017-06-06 12:37:20 +0200

    Add user id to children (#603)
    * Added user counts to network (#601)
    
    * Added user counts to network
    
    * Added assert to check for users_count
    
    * Bugfix/dont count deleted users (#602)
    
    * Don't count users which have been removed
    
    * Added test to see if deleted users show up in users count
    
    * Renamed variable
    
    * Fixed typo
    
    * Optionally include deleted users in count too ( default false )
    
    * Add userId to children objects

--- source/modules/core/handlers/networks-for-organisation.e2e.js
+++ source/modules/core/handlers/networks-for-organisation.e2e.js
@@ -2,31 +2,37 @@ const { assert } = require('chai');
 const testHelpers = require('../../../shared/test-utils/helpers');
 const { getRequest } = require('../../../shared/test-utils/request');
 const organisationRepo = require('../repositories/organisation');
+const networkRepo = require('../repositories/network');
 
 describe('Handler: Networks for organisation', () => {
   let organisation;
   let admin;
   let otherUser;
+  let userToBeDeleted;
 
   before(async () => {
-    [organisation, admin, otherUser] = await Promise.all([
+    [organisation, admin, otherUser, userToBeDeleted] = await Promise.all([
       testHelpers.createOrganisation(),
       testHelpers.createUser(),
       testHelpers.createUser(),
+      testHelpers.createUser(),
     ]);
 
 
-    return Promise.all([
+    const [network] = await Promise.all([
+      testHelpers.createNetwork({
+        userId: admin.id,
+        organisationId: organisation.id,
+      }),
+      testHelpers.createNetwork({
+        userId: admin.id,
+        organisationId: organisation.id,
+      }),
       organisationRepo.addUser(admin.id, organisation.id, 'ADMIN'),
-      testHelpers.createNetwork({
-        userId: admin.id,
-        organisationId: organisation.id,
-      }),
-      testHelpers.createNetwork({
-        userId: admin.id,
-        organisationId: organisation.id,
-      }),
     ]);
+
+    await testHelpers.addUserToNetwork({ networkId: network.id, userId: userToBeDeleted.id });
+    await networkRepo.removeUser(network.id, userToBeDeleted.id);
   });
 
   after(() => testHelpers.cleanAll());
@@ -36,6 +42,7 @@ describe('Handler: Networks for organisation', () => {
     const { result } = await getRequest(endpoint, admin.token);
 
     assert.lengthOf(result.data, 2);
+    assert.equal(result.data[0].users_count, 1);
   });
 
   it('should fail when user doesnt belong to the organisation', async () => {
